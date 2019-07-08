21be2897dc116f49ffd0e9b2214244dede9a7872
Florian Limpoeck <florian.limpoeck@gmail.com>
2019-07-08 14:14:23 +0200

    added license headers.

--- src/extensionApi/java/com/hivemq/extension/sdk/api/packets/subscribe/SubscribePacket.java
+++ src/extensionApi/java/com/hivemq/extension/sdk/api/packets/subscribe/SubscribePacket.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 dc-square GmbH
+ * Copyright 2019 dc-square GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
