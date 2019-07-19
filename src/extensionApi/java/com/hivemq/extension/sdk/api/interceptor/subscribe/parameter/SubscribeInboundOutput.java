160766bc373d16ca740d605e66a4dc63e3fb7e20
Florian Limpoeck <florian.limpoeck@gmail.com>
2019-07-19 17:37:06 +0200

    updated async java doc.

--- src/extensionApi/java/com/hivemq/extension/sdk/api/interceptor/subscribe/parameter/SubscribeInboundOutput.java
+++ src/extensionApi/java/com/hivemq/extension/sdk/api/interceptor/subscribe/parameter/SubscribeInboundOutput.java
@@ -18,9 +18,14 @@ package com.hivemq.extension.sdk.api.interceptor.subscribe.parameter;
 
 import com.hivemq.extension.sdk.api.annotations.DoNotImplement;
 import com.hivemq.extension.sdk.api.annotations.NotNull;
+import com.hivemq.extension.sdk.api.async.Async;
 import com.hivemq.extension.sdk.api.async.AsyncOutput;
+import com.hivemq.extension.sdk.api.async.TimeoutFallback;
 import com.hivemq.extension.sdk.api.interceptor.subscribe.SubscribeInboundInterceptor;
 import com.hivemq.extension.sdk.api.packets.subscribe.ModifiableSubscribePacket;
+import com.hivemq.extension.sdk.api.packets.subscribe.SubackReasonCode;
+
+import java.time.Duration;
 
 /**
  * This is the output parameter of any {@link SubscribeInboundInterceptor}
@@ -43,5 +48,38 @@ public interface SubscribeInboundOutput extends AsyncOutput<SubscribeInboundOutp
      */
     @NotNull
     ModifiableSubscribePacket getSubscribePacket();
+
+    /**
+     * If the timeout is expired before {@link Async#resume()} is called then the outcome is
+     * handled either as failed or successful, depending on the specified fallback.
+     * <p>
+     * Do not call this method more than once. If an async method is called multiple times an exception is thrown.
+     * <p>
+     * {@link TimeoutFallback#FAILURE} results in a Suback with {@link SubackReasonCode#UNSPECIFIED_ERROR} and the
+     * subscription will be prevented.
+     * <p>
+     * {@link TimeoutFallback#SUCCESS} will proceed the subscription.
+     *
+     * @param timeout  Timeout that HiveMQ waits for the result of the async operation.
+     * @param fallback Fallback behaviour if a timeout occurs.
+     * @throws UnsupportedOperationException If async is called more than once.
+     * @since 4.2.0
+     */
+    @Override
+    @NotNull Async<SubscribeInboundOutput> async(@NotNull Duration timeout, @NotNull TimeoutFallback fallback);
+
+    /**
+     * If the timeout is expired before {@link Async#resume()} is called then the outcome is handled as failed.
+     * This means that the outcome results in a Suback with {@link SubackReasonCode#UNSPECIFIED_ERROR} and the
+     * subscription will be prevented.
+     * <p>
+     * Do not call this method more than once. If an async method is called multiple times an exception is thrown.
+     *
+     * @param timeout Timeout that HiveMQ waits for the result of the async operation.
+     * @throws UnsupportedOperationException If async is called more than once.
+     * @since 4.2.0
+     */
+    @Override
+    @NotNull Async<SubscribeInboundOutput> async(@NotNull Duration timeout);
 
 }
