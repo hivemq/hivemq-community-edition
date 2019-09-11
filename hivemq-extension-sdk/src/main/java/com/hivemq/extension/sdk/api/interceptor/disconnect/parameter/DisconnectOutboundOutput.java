8e22e7faea1ba1b9782788dd8a313c0778dc89f7
ratherto <robinatherton1995@gmail.com>
2019-09-11 08:32:32 +0200

    Removed async method

--- hivemq-extension-sdk/src/main/java/com/hivemq/extension/sdk/api/interceptor/disconnect/parameter/DisconnectOutboundOutput.java
+++ hivemq-extension-sdk/src/main/java/com/hivemq/extension/sdk/api/interceptor/disconnect/parameter/DisconnectOutboundOutput.java
@@ -2,12 +2,8 @@ package com.hivemq.extension.sdk.api.interceptor.disconnect.parameter;
 
 import com.hivemq.extension.sdk.api.annotations.DoNotImplement;
 import com.hivemq.extension.sdk.api.annotations.NotNull;
-import com.hivemq.extension.sdk.api.async.Async;
-import com.hivemq.extension.sdk.api.async.TimeoutFallback;
 import com.hivemq.extension.sdk.api.interceptor.disconnect.DisconnectOutboundInterceptor;
 import com.hivemq.extension.sdk.api.packets.disconnect.ModifiableDisconnectPacket;
-
-import java.time.Duration;
 
 /**
  * This is the output parameter of any {@link DisconnectOutboundInterceptor} providing methods to define the outcome of
@@ -25,20 +21,5 @@ public interface DisconnectOutboundOutput {
      */
     @NotNull ModifiableDisconnectPacket getDisconnectPacket();
 
-    /**
-     * If the timeout expires before {@link Async#resume()} is called then the outcome is handled either as failed or
-     * successful, depending on the specified fallback.
-     * <p>
-     * Do not call this method more than once. If an async method is called multiple times an exception is thrown.
-     * <p>
-     * {@link TimeoutFallback#FAILURE} results in closed connection without a DISCONNECT sent to the client.
-     * <p>
-     * {@link TimeoutFallback#SUCCESS} will proceed the DISCONNECT.
-     *
-     * @param timeout Timeout that HiveMQ waits for the result of the async operation.
-     * @param fallback Fallback behavior if a timeout occurs.
-     * @throws UnsupportedOperationException if async is called more than once.
-     */
-    @NotNull Async<DisconnectOutboundOutput> async(@NotNull Duration timeout, @NotNull TimeoutFallback fallback);
 
 }
