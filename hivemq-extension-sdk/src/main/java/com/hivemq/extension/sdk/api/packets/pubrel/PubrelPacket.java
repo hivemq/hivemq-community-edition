a6364959d636d0e14f3b244e861e8bf08c87ba9e
yweber <yannick.weber95@googlemail.com>
2019-10-07 14:32:47 +0200

    removed confusing annotation added javadoc to getPacketIdentifier

--- hivemq-extension-sdk/src/main/java/com/hivemq/extension/sdk/api/packets/pubrel/PubrelPacket.java
+++ hivemq-extension-sdk/src/main/java/com/hivemq/extension/sdk/api/packets/pubrel/PubrelPacket.java
@@ -15,6 +15,12 @@ import java.util.Optional;
  */
 public interface PubrelPacket {
 
+    /**
+     * The packet identifier of the PUBREL.
+     *
+     * @return The packet identifier.
+     * @since 4.3.0
+     */
     int getPacketIdentifier();
 
     /**
