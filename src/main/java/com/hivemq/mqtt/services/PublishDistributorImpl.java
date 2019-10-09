df51032f35bfbf0b88ca382e753d87c019774fe6
Florian Limpoeck <florian.limpoeck@gmail.com>
2019-10-09 15:25:57 +0200

    removed unused import

--- src/main/java/com/hivemq/mqtt/services/PublishDistributorImpl.java
+++ src/main/java/com/hivemq/mqtt/services/PublishDistributorImpl.java
@@ -40,7 +40,6 @@ import javax.inject.Singleton;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 
 import static com.hivemq.mqtt.handler.publish.PublishStatus.*;
