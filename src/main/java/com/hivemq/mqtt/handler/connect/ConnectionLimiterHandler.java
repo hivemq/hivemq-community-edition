f715905c3913bd6cb66be26df34cdf452bde760c
yweber <yannick.weber95@googlemail.com>
2020-01-20 15:48:35 +0100

    added ConnectionLimiter

---
+++ src/main/java/com/hivemq/mqtt/handler/connect/ConnectionLimiterHandler.java
@@ -0,0 +1,102 @@
+package com.hivemq.mqtt.handler.connect;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.hivemq.annotations.NotNull;
+import com.hivemq.configuration.service.RestrictionsConfigurationService;
+import com.hivemq.logging.EventLog;
+import com.hivemq.metrics.gauges.OpenConnectionsGauge;
+import com.hivemq.mqtt.message.connect.CONNECT;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * A {@link ChannelHandler} which is responsible for limiting the concurrent connections
+ * which is defined by the 'max-connections' parameter in the configuration.
+ *
+ * @author Yannick Weber
+ */
+@Singleton
+@ChannelHandler.Sharable
+public class ConnectionLimiterHandler extends ChannelInboundHandlerAdapter {
+
+    private final static Logger log = LoggerFactory.getLogger(ConnectionLimiterHandler.class);
+
+    private final @NotNull EventLog eventLog;
+    private final @NotNull RestrictionsConfigurationService restrictionsConfigurationService;
+    private final @NotNull OpenConnectionsGauge openConnectionsGauge;
+    private volatile long maxConnections;
+    private volatile long warnThreshold;
+
+    @Inject
+    public ConnectionLimiterHandler(
+            final @NotNull EventLog eventLog,
+            final @NotNull RestrictionsConfigurationService restrictionsConfigurationService,
+            final @NotNull OpenConnectionsGauge openConnectionsGauge) {
+        this.eventLog = eventLog;
+        this.restrictionsConfigurationService = restrictionsConfigurationService;
+        this.openConnectionsGauge = openConnectionsGauge;
+    }
+
+    @Override
+    public void channelActive(final @NotNull ChannelHandlerContext ctx) throws Exception {
+
+        final long configuredCount = restrictionsConfigurationService.maxConnections();
+
+        if (configuredCount > RestrictionsConfigurationService.UNLIMITED_CONNECTIONS) {
+            // If we use the max connections configured in the config file, we set the Threshold to 90% of the maximum allowed connections.
+            this.warnThreshold = 90 * configuredCount / 100;
+            this.maxConnections = configuredCount;
+            if (log.isDebugEnabled()) {
+                log.debug("The connection limit is set to ({}), the warn threshold is set to ({}).", this.maxConnections, this.warnThreshold);
+            }
+        } else {
+            //This means we are dealing with unlimited connections so we can remove this handler from the pipeline
+            if (log.isDebugEnabled()) {
+                log.debug("An unlimited amount of connections (-1) is configured.");
+            }
+            ctx.pipeline().remove(this);
+        }
+
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelRead(final @NotNull ChannelHandlerContext ctx, final @NotNull Object msg) throws Exception {
+        if (msg instanceof CONNECT) {
+
+            final CONNECT connect = (CONNECT) msg;
+
+            final long currentCount = openConnectionsGauge.getValue();
+
+            if (currentCount > maxConnections) {
+                log.warn("The connection limit ({}) is reached. ClientID ({}) connection denied.", maxConnections, connect.getClientIdentifier());
+                eventLog.clientWasDisconnected(ctx.channel(),"The configured maximum amount of connections is reached.");
+                ctx.close();
+                return;
+            } else if (warnThreshold > 0 && currentCount >= warnThreshold) {
+                log.warn("The amount of connections ({}) is close to its limit ({}).", currentCount, maxConnections);
+            }
+
+            // We can remove the handler because it doesn't do anything after this point.
+            ctx.pipeline().remove(this);
+        }
+        super.channelRead(ctx, msg);
+    }
+
+    @VisibleForTesting
+    long getWarnThreshold() {
+        return warnThreshold;
+    }
+
+    @VisibleForTesting
+    long getMaxConnections() {
+        return maxConnections;
+    }
+}
+
---
+++ src/test/java/com/hivemq/mqtt/handler/connect/ConnectionLimiterHandlerTest.java
@@ -0,0 +1,90 @@
+package com.hivemq.mqtt.handler.connect;
+
+import com.hivemq.configuration.service.RestrictionsConfigurationService;
+import com.hivemq.logging.EventLog;
+import com.hivemq.metrics.gauges.OpenConnectionsGauge;
+import com.hivemq.mqtt.message.connect.CONNECT;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPipeline;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Yannick Weber
+ */
+@SuppressWarnings("NullabilityAnnotations")
+public class ConnectionLimiterHandlerTest {
+
+    @Mock
+    OpenConnectionsGauge connectionCounter;
+
+    @Mock
+    ChannelHandlerContext ctx;
+
+    @Mock
+    ChannelPipeline pipeline;
+
+    @Mock
+    Channel channel;
+
+    @Mock
+    EventLog eventLog;
+
+    @Mock
+    RestrictionsConfigurationService restrictionsEntity;
+
+    @Mock
+    CONNECT connect;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        when(ctx.channel()).thenReturn(channel);
+        when(ctx.pipeline()).thenReturn(pipeline);
+    }
+
+    @Test
+    public void exceed_maximum_connections() throws Exception {
+        when(restrictionsEntity.maxConnections()).thenReturn(10L);
+        when(connectionCounter.getValue()).thenReturn(11);
+        final ConnectionLimiterHandler limiter =
+                new ConnectionLimiterHandler(eventLog, restrictionsEntity, connectionCounter);
+        limiter.channelActive(ctx);
+        limiter.channelRead(ctx, connect);
+        assertEquals(10L, limiter.getMaxConnections());
+        assertEquals(9L, limiter.getWarnThreshold());
+        verify(eventLog).clientWasDisconnected(any(Channel.class), anyString());
+        verify(ctx, atLeastOnce()).close();
+    }
+
+    @Test
+    public void under_maximum_connections() throws Exception {
+        when(restrictionsEntity.maxConnections()).thenReturn(4L);
+        when(connectionCounter.getValue()).thenReturn(3);
+        final ConnectionLimiterHandler limiter =
+                new ConnectionLimiterHandler(eventLog, restrictionsEntity, connectionCounter);
+        limiter.channelActive(ctx);
+        limiter.channelRead(ctx, connect);
+        assertEquals(4L, limiter.getMaxConnections());
+        verify(ctx, never()).close();
+    }
+
+    @Test
+    public void unlimited_connections() throws Exception {
+        when(restrictionsEntity.maxConnections()).thenReturn(-1L);
+        when(connectionCounter.getValue()).thenReturn(11);
+        final ConnectionLimiterHandler limiter =
+                new ConnectionLimiterHandler(eventLog, restrictionsEntity, connectionCounter);
+        limiter.channelActive(ctx);
+        verify(pipeline, atLeastOnce()).remove(any(ChannelHandler.class));
+    }
+
+}
