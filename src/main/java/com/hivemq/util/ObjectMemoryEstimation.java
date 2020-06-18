7fbcab814e15c378e755f53581f780666f511fd8
Florian Limpoeck <florian.limpoeck@gmail.com>
2020-06-18 16:42:26 +0200

    addressed latest PR comments.

--- src/main/java/com/hivemq/util/ObjectMemoryEstimation.java
+++ src/main/java/com/hivemq/util/ObjectMemoryEstimation.java
@@ -28,7 +28,7 @@ public class ObjectMemoryEstimation {
     public static final int STRING_OVERHEAD = 38;
     public static final int ARRAY_OVERHEAD = 12;
     public static final int COLLECTION_OVERHEAD = 12;
-    public static final int LONG_WRAPPER_SIZE = 20;
+    public static final int LONG_WRAPPER_SIZE = 24;
     public static final int INT_WRAPPER_SIZE = 16;
     public static final int LONG_SIZE = 8;
     public static final int INT_SIZE = 4;
