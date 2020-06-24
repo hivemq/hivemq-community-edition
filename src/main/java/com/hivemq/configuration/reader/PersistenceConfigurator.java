e908b4d26477edee9d9f64b045792ef4001aff60
Silvio Giebl <silvio.giebl@hivemq.com>
2020-06-24 15:45:23 +0200

    Fixed rebase

--- src/main/java/com/hivemq/configuration/reader/PersistenceConfigurator.java
+++ src/main/java/com/hivemq/configuration/reader/PersistenceConfigurator.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2019 dc-square GmbH
+ * Copyright 2019-present HiveMQ GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,7 +17,7 @@ package com.hivemq.configuration.reader;
 
 import com.hivemq.configuration.entity.PersistenceEntity;
 import com.hivemq.configuration.service.PersistenceConfigurationService;
-import org.jetbrains.annotations.NotNull;
+import com.hivemq.extension.sdk.api.annotations.NotNull;
 
 /**
  * @author Lukas Brandl
