1d83f738d6901559954f8636d5fc1a1707d27480
Georg <georg.held@hivemq.com>
2020-07-03 17:18:34 +0200

    Ported the iteration unit tests.

---
+++ src/test/java/com/hivemq/extensions/iteration/ChunkerTest.java
@@ -0,0 +1,103 @@
+package com.hivemq.extensions.iteration;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.hivemq.configuration.service.InternalConfigurations;
+import com.hivemq.extension.sdk.api.annotations.NotNull;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class ChunkerTest {
+
+
+    private int bucketCount;
+
+    @Before
+    public void setUp() throws Exception {
+        bucketCount = InternalConfigurations.PERSISTENCE_BUCKET_COUNT.get();
+        InternalConfigurations.PERSISTENCE_BUCKET_COUNT.set(4);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        InternalConfigurations.PERSISTENCE_BUCKET_COUNT.set(bucketCount);
+
+    }
+
+    @Test
+    public void calculatesMaxResults() {
+        final Chunker chunker = new Chunker();
+
+        chunker.getAllLocalChunk(new ChunkCursor(), 4, new Chunker.SingleWriterCall<String>() {
+            @Override
+            public ListenableFuture<@NotNull BucketChunkResult<Map<String, String>>> call(final int bucket, final @NotNull String lastKey, final int maxResults) {
+                assertEquals(1, maxResults);
+                return Futures.immediateFuture(new BucketChunkResult<>(Map.of(), true, "last", bucket));
+            }
+        });
+    }
+
+    @Test
+    public void callsAllBuckets() throws Exception {
+        final int[] counter = {0};
+        final Chunker chunker = new Chunker();
+
+        final MultipleChunkResult<Map<String, @NotNull String>> multi = chunker.getAllLocalChunk(new ChunkCursor(), 4, new Chunker.SingleWriterCall<String>() {
+            @Override
+            public ListenableFuture<@NotNull BucketChunkResult<Map<String, String>>> call(final int bucket, final @NotNull String lastKey, final int maxResults) {
+
+                counter[0]++;
+                return Futures.immediateFuture(new BucketChunkResult<>(Map.of(), false, "last", bucket));
+            }
+        }).get();
+
+        final Map<Integer, BucketChunkResult<Map<String, @NotNull String>>> values = multi.getValues();
+        assertEquals(4, values.size());
+        final Set<Integer> buckets = values.keySet();
+        assertTrue(buckets.contains(0));
+        assertTrue(buckets.contains(1));
+        assertTrue(buckets.contains(2));
+        assertTrue(buckets.contains(3));
+        assertFalse(values.get(0).isFinished());
+        assertFalse(values.get(1).isFinished());
+        assertFalse(values.get(2).isFinished());
+        assertFalse(values.get(3).isFinished());
+        assertEquals(4, counter[0]);
+    }
+
+    @Test
+    public void doesNotCallFinishedBuckets() throws Exception {
+        final int[] counter = {0};
+        final Chunker chunker = new Chunker();
+
+        final ChunkCursor cursor = new ChunkCursor(new HashMap<>(), ImmutableSet.of(1, 3));
+        final MultipleChunkResult<Map<String, @NotNull String>> multi = chunker.getAllLocalChunk(cursor, 4, new Chunker.SingleWriterCall<String>() {
+            @Override
+            public ListenableFuture<@NotNull BucketChunkResult<Map<String, String>>> call(final int bucket, final @NotNull String lastKey, final int maxResults) {
+                counter[0]++;
+                return Futures.immediateFuture(new BucketChunkResult<>(Map.of(), false, "last", bucket));
+            }
+        }).get();
+
+        final Map<Integer, BucketChunkResult<Map<String, @NotNull String>>> values = multi.getValues();
+        assertEquals(4, values.size());
+        final Set<Integer> buckets = values.keySet();
+        assertTrue(buckets.contains(0));
+        assertTrue(buckets.contains(1));
+        assertTrue(buckets.contains(2));
+        assertTrue(buckets.contains(3));
+        assertFalse(values.get(0).isFinished());
+        assertTrue(values.get(1).isFinished());
+        assertFalse(values.get(2).isFinished());
+        assertTrue(values.get(3).isFinished());
+        assertEquals(2, counter[0]);
+    }
+}
