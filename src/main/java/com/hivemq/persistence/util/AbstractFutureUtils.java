283fc5a18808127a74719381cd096b4498d60ab0
A-Imal <Abdullah.Imal@dc-square.de>
2020-07-07 22:02:37 +0200

    Future utils do now return a result future instead of receiving one as argument and setting it. Removed unused methods.

--- src/main/java/com/hivemq/persistence/util/AbstractFutureUtils.java
+++ src/main/java/com/hivemq/persistence/util/AbstractFutureUtils.java
@@ -13,8 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.hivemq.persistence.util;
 
+import com.google.common.base.Function;
 import com.google.common.collect.ImmutableList;
 import com.google.common.util.concurrent.*;
 import com.hivemq.extension.sdk.api.annotations.NotNull;
@@ -24,8 +26,8 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 
 /**
@@ -34,167 +36,87 @@ import java.util.concurrent.ExecutorService;
 public abstract class AbstractFutureUtils {
 
     private static final Logger log = LoggerFactory.getLogger(AbstractFutureUtils.class);
+    private static final AnyToVoidFunction ANY_TO_VOID_FUNCTION = new AnyToVoidFunction();
 
+    public abstract @NotNull ListenableFuture<Void> voidFutureFromAnyFuture(@NotNull ListenableFuture<?> anyFuture);
 
-    public abstract ListenableFuture<Void> voidFutureFromAnyFuture(final ListenableFuture<?> anyFuture);
-
-
-    public ListenableFuture<Void> voidFutureFromAnyFuture(final ListenableFuture<?> anyFuture, final ExecutorService executorService) {
-        return Futures.transformAsync(anyFuture, new AnyToVoidAsyncFunction(), executorService);
+    public @NotNull ListenableFuture<Void> voidFutureFromAnyFuture(
+            final @NotNull ListenableFuture<?> anyFuture, final @NotNull Executor executor) {
+        return Futures.transform(anyFuture, ANY_TO_VOID_FUNCTION, executor);
     }
 
-    public ListenableFuture<Void> mergeVoidFutures(final ListenableFuture<Void> future1, final ListenableFuture<Void> future2) {
+    public @NotNull ListenableFuture<Void> mergeVoidFutures(
+            final @NotNull ListenableFuture<Void> future1, final @NotNull ListenableFuture<Void> future2) {
         return voidFutureFromList(ImmutableList.of(future1, future2));
     }
 
-    public ListenableFuture<Void> voidFutureFromList(final ImmutableList<ListenableFuture<Void>> futureList) {
+    public <T> @NotNull ListenableFuture<Void> voidFutureFromList(final @NotNull ImmutableList<ListenableFuture<T>> futureList) {
 
         if (futureList.isEmpty()) {
             return Futures.immediateFuture(null);
         }
 
         final SettableFuture<Void> resultFuture = SettableFuture.create();
-        final FutureCallback<Void> futureCallback = new VoidFutureCombiningCallback(futureList.size(), resultFuture);
-        for (final ListenableFuture<Void> voidListenableFuture : futureList) {
+        final FutureCallback<T> futureCallback = new VoidFutureCombiningCallback<>(futureList.size(), resultFuture);
 
+        for (final ListenableFuture<T> voidListenableFuture : futureList) {
             Futures.addCallback(voidListenableFuture, futureCallback, MoreExecutors.directExecutor());
         }
 
         return resultFuture;
     }
 
-    public ListenableFuture<Void> voidFutureFromAnyFutureList(final ImmutableList<ListenableFuture> futureList) {
-
-        if (futureList.isEmpty()) {
-            return Futures.immediateFuture(null);
-        }
-
-        final SettableFuture<Void> resultFuture = SettableFuture.create();
-        final FutureCallback<Void> futureCallback = new VoidFutureFromAnyCombiningCallback(futureList.size(), resultFuture);
-        for (final ListenableFuture voidListenableFuture : futureList) {
-
-            Futures.addCallback(voidListenableFuture, futureCallback, MoreExecutors.directExecutor());
-        }
-
-        return resultFuture;
-    }
-
-    public <T> void addSettableFutureCallback(final ListenableFuture<T> listenableFuture, final SettableFuture<T> settableFuture) {
+    public <T> void addSettableFutureCallback(
+            final @NotNull ListenableFuture<T> listenableFuture, final @NotNull SettableFuture<T> settableFuture) {
         Futures.addCallback(listenableFuture, new FutureCallback<T>() {
             @Override
-            public void onSuccess(final T result) {
+            public void onSuccess(final @Nullable T result) {
                 settableFuture.set(result);
             }
 
             @Override
-            public void onFailure(final Throwable throwable) {
-                settableFuture.setException(throwable);
-            }
-        }, MoreExecutors.directExecutor());
-
-    }
-
-    public <T> ListenableFuture<T> futureOfFuture(final ListenableFuture<ListenableFuture<T>> futureOfFuture) {
-
-        final SettableFuture<T> resultFuture = SettableFuture.create();
-
-        Futures.addCallback(futureOfFuture, new FutureCallback<>() {
-            @Override
-            public void onSuccess(@Nullable final ListenableFuture<T> result) {
-                if (result == null) {
-                    resultFuture.set(null);
-                    return;
-                }
-
-                addSettableFutureCallback(result, resultFuture);
-            }
-
-            @Override
-            public void onFailure(final Throwable t) {
-                resultFuture.setException(t);
-            }
-        }, MoreExecutors.directExecutor());
-
-        return resultFuture;
-    }
-
-    public <T> void addSettableFutureCallback(final ListenableFuture<Void> listenableFuture, final SettableFuture<T> settableFuture, final T result) {
-        Futures.addCallback(listenableFuture, new FutureCallback<Void>() {
-            @Override
-            public void onSuccess(final Void aVoid) {
-                settableFuture.set(result);
-            }
-
-            @Override
-            public void onFailure(final Throwable throwable) {
+            public void onFailure(final @NotNull Throwable throwable) {
                 settableFuture.setException(throwable);
             }
         }, MoreExecutors.directExecutor());
     }
 
-    public void addExceptionLogger(final ListenableFuture<?> listenableFuture) {
+    public void addExceptionLogger(final @NotNull ListenableFuture<?> listenableFuture) {
         Futures.addCallback(listenableFuture, new FutureCallback<Object>() {
             @Override
-            public void onSuccess(@Nullable final Object o) {
+            public void onSuccess(final @Nullable Object o) {
                 //no op
             }
 
             @Override
-            public void onFailure(final Throwable throwable) {
+            public void onFailure(final @NotNull Throwable throwable) {
                 log.error("Uncaught exception", throwable);
             }
         }, MoreExecutors.directExecutor());
     }
 
-    public <T> void addPersistenceCallback(final ListenableFuture<T> future, final FutureCallback<? super T> callback, final ExecutorService executorService) {
+    public <T> void addPersistenceCallback(
+            final @NotNull ListenableFuture<T> future,
+            final @NotNull FutureCallback<? super T> callback,
+            final @NotNull ExecutorService executorService) {
         if (!executorService.isShutdown()) {
             Futures.addCallback(future, callback, executorService);
         }
     }
 
-    public abstract <T> void addPersistenceCallback(final ListenableFuture<T> future, final FutureCallback<? super T> callback);
+    public abstract <T> void addPersistenceCallback(
+            @NotNull ListenableFuture<T> future, @NotNull FutureCallback<? super T> callback);
 
-    public abstract <T, S> void multiFutureResult(@NotNull final Map<ListenableFuture<T>, FutureUtils.Function<T>> futures, final S resultSum, final SettableFuture<S> resultSumFuture);
+    public abstract <E, C extends Collection<Set<E>>> @NotNull ListenableFuture<Set<E>> combineSetResults(
+            @NotNull ListenableFuture<C> collectionFuture);
 
-    public <T, S> void multiFutureResult(@NotNull final Map<ListenableFuture<T>, FutureUtils.Function<T>> futures, final S resultSum, final SettableFuture<S> resultSumFuture, final ExecutorService executorService) {
-        final ImmutableList.Builder<ListenableFuture<Void>> builder = ImmutableList.builder();
-        for (final ListenableFuture<T> future : futures.keySet()) {
-            final SettableFuture<Void> resultFuture = SettableFuture.create();
-            builder.add(resultFuture);
-            Futures.addCallback(future, new FutureCallback<T>() {
-                @Override
-                public void onSuccess(@Nullable final T result) {
-                    futures.get(future).function(result);
-                    resultFuture.set(null);
-                }
+    public <E, C extends Collection<Set<E>>> @NotNull ListenableFuture<Set<E>> combineSetResults(
+            final @NotNull ListenableFuture<C> collectionFuture, final @NotNull Executor executor) {
 
-                @Override
-                public void onFailure(final Throwable t) {
-                    resultFuture.setException(t);
-                }
-            }, executorService);
-        }
-        final ListenableFuture<Void> doneFuture = FutureUtils.voidFutureFromList(builder.build());
-        Futures.addCallback(doneFuture, new FutureCallback<Void>() {
-            @Override
-            public void onSuccess(@Nullable final Void result) {
-                resultSumFuture.set(resultSum);
-            }
-
-            @Override
-            public void onFailure(final Throwable t) {
-                resultSumFuture.setException(t);
-            }
-        }, MoreExecutors.directExecutor());
-    }
-
-    public abstract <E, C extends Collection<Set<E>>> ListenableFuture<Set<E>> combineSetResults(final ListenableFuture<C> collectionFuture);
-
-    public <E, C extends Collection<Set<E>>> ListenableFuture<Set<E>> combineSetResults(final ListenableFuture<C> collectionFuture, final ExecutorService executorService) {
         final SettableFuture<Set<E>> resultFuture = SettableFuture.create();
         Futures.addCallback(collectionFuture, new FutureCallback<C>() {
             @Override
-            public void onSuccess(@Nullable final C result) {
+            public void onSuccess(final @Nullable C result) {
                 if (result == null) {
                     resultFuture.set(null);
                     return;
@@ -207,36 +129,18 @@ public abstract class AbstractFutureUtils {
             }
 
             @Override
-            public void onFailure(final Throwable t) {
+            public void onFailure(final @NotNull Throwable t) {
                 resultFuture.setException(t);
             }
-        }, executorService);
+        }, executor);
         return resultFuture;
     }
 
-    private static class AnyToVoidAsyncFunction implements AsyncFunction<Object, Void> {
-        @Override
-        public ListenableFuture<Void> apply(@Nullable final Object input) throws Exception {
-            return Futures.immediateFuture(null);
-        }
-    }
-
-    public ListenableFuture<Boolean> voidToBoolFuture(final ListenableFuture<Void> future, final boolean value) {
-        return Futures.transformAsync(future, new VoidToBooleanAsyncFunction(value), MoreExecutors.newDirectExecutorService());
-    }
-
-    private static class VoidToBooleanAsyncFunction implements AsyncFunction<Void, Boolean> {
-
-        private final boolean value;
-
-        private VoidToBooleanAsyncFunction(final boolean value) {
-            this.value = value;
-        }
+    private static class AnyToVoidFunction implements Function<Object, Void> {
 
         @Override
-        public ListenableFuture<Boolean> apply(@Nullable final Void input) throws Exception {
-            return Futures.immediateFuture(value);
+        public Void apply(final @Nullable Object input) {
+            return null;
         }
     }
-
 }
--- src/main/java/com/hivemq/persistence/util/FutureUtils.java
+++ src/main/java/com/hivemq/persistence/util/FutureUtils.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.hivemq.persistence.util;
 
 import com.google.common.collect.ImmutableList;
@@ -23,9 +24,7 @@ import com.google.inject.Inject;
 import com.hivemq.extension.sdk.api.annotations.NotNull;
 
 import java.util.Collection;
-import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ExecutorService;
 
 /**
  * @author Lukas Brandl
@@ -35,67 +34,35 @@ public class FutureUtils {
     @Inject
     public static AbstractFutureUtils delegate;
 
-    public static ListenableFuture<Void> voidFutureFromAnyFuture(final ListenableFuture<?> anyFuture) {
+    public static @NotNull ListenableFuture<Void> voidFutureFromAnyFuture(final @NotNull ListenableFuture<?> anyFuture) {
         return delegate.voidFutureFromAnyFuture(anyFuture);
     }
 
-    public static ListenableFuture<Void> voidFutureFromAnyFuture(final ListenableFuture<?> anyFuture, final ExecutorService executorService) {
-        return delegate.voidFutureFromAnyFuture(anyFuture, executorService);
-    }
-
-    public static ListenableFuture<Void> mergeVoidFutures(final ListenableFuture<Void> future1, final ListenableFuture<Void> future2) {
+    public static @NotNull ListenableFuture<Void> mergeVoidFutures(
+            final @NotNull ListenableFuture<Void> future1, final @NotNull ListenableFuture<Void> future2) {
         return delegate.mergeVoidFutures(future1, future2);
     }
 
-    public static ListenableFuture<Void> voidFutureFromList(final ImmutableList<ListenableFuture<Void>> futureList) {
+    public static <T> @NotNull ListenableFuture<Void> voidFutureFromList(final @NotNull ImmutableList<ListenableFuture<T>> futureList) {
         return delegate.voidFutureFromList(futureList);
     }
 
-    public static ListenableFuture<Void> voidFutureFromAnyFutureList(final ImmutableList<ListenableFuture> futureList) {
-        return delegate.voidFutureFromAnyFutureList(futureList);
-    }
-
-    public static <T> void addSettableFutureCallback(final ListenableFuture<T> listenableFuture, final SettableFuture<T> settableFuture) {
+    public static <T> void addSettableFutureCallback(
+            final @NotNull ListenableFuture<T> listenableFuture, final @NotNull SettableFuture<T> settableFuture) {
         delegate.addSettableFutureCallback(listenableFuture, settableFuture);
     }
 
-    public static <T> ListenableFuture<T> futureOfFuture(final ListenableFuture<ListenableFuture<T>> futureOfFuture) {
-        return delegate.futureOfFuture(futureOfFuture);
-    }
-
-    public static <T> void addSettableFutureCallback(final ListenableFuture<Void> listenableFuture, final SettableFuture<T> settableFuture, final T result) {
-        delegate.addSettableFutureCallback(listenableFuture, settableFuture, result);
-    }
-
-    public static void addExceptionLogger(final ListenableFuture<?> listenableFuture) {
+    public static void addExceptionLogger(final @NotNull ListenableFuture<?> listenableFuture) {
         delegate.addExceptionLogger(listenableFuture);
     }
 
-    public static <T> void addPersistenceCallback(final ListenableFuture<T> future, final FutureCallback<? super T> callback) {
+    public static <T> void addPersistenceCallback(
+            final @NotNull ListenableFuture<T> future, final @NotNull FutureCallback<? super T> callback) {
         delegate.addPersistenceCallback(future, callback);
     }
 
-    public static <T, S> void multiFutureResult(@NotNull final Map<ListenableFuture<T>, Function<T>> futures, final S resultSum, final SettableFuture<S> resultSumFuture) {
-        delegate.multiFutureResult(futures, resultSum, resultSumFuture);
-    }
-
-    public static <T, S> void multiFutureResult(@NotNull final Map<ListenableFuture<T>, Function<T>> futures, final S resultSum, final SettableFuture<S> resultSumFuture, final ExecutorService executorService) {
-        delegate.multiFutureResult(futures, resultSum, resultSumFuture, executorService);
-    }
-
-    public static <E, C extends Collection<Set<E>>> ListenableFuture<Set<E>> combineSetResults(final ListenableFuture<C> collectionFuture) {
+    public static <E, C extends Collection<Set<E>>> @NotNull ListenableFuture<Set<E>> combineSetResults(
+            final @NotNull ListenableFuture<C> collectionFuture) {
         return delegate.combineSetResults(collectionFuture);
-    }
-
-    public static <E, C extends Collection<Set<E>>> ListenableFuture<Set<E>> combineSetResults(final ListenableFuture<C> collectionFuture, final ExecutorService executorService) {
-        return delegate.combineSetResults(collectionFuture, executorService);
-    }
-
-    public interface Function<T> {
-        void function(final T result);
-    }
-
-    public static ListenableFuture<Boolean> voidToBoolFuture(final ListenableFuture<Void> future, final boolean value) {
-        return delegate.voidToBoolFuture(future, value);
     }
 }
