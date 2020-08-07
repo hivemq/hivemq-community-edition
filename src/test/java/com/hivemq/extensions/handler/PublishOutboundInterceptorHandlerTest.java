4190044295e4d217b0593bc85980a09eb3262ba2
Christoph Schaebel <christoph.schaebel@dc-square.de>
2020-08-07 14:09:54 +0200

    improve preventDelivery on PublishOutboundInterceptor

--- src/test/java/com/hivemq/extensions/handler/PublishOutboundInterceptorHandlerTest.java
+++ src/test/java/com/hivemq/extensions/handler/PublishOutboundInterceptorHandlerTest.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.hivemq.extensions.handler;
 
 import com.google.common.collect.ImmutableList;
@@ -26,11 +27,15 @@ import com.hivemq.extensions.classloader.IsolatedPluginClassloader;
 import com.hivemq.extensions.client.ClientContextImpl;
 import com.hivemq.extensions.executor.PluginOutPutAsyncer;
 import com.hivemq.extensions.executor.PluginTaskExecutorService;
+import com.hivemq.extensions.interceptor.publish.parameter.PublishOutboundOutputImpl;
+import com.hivemq.mqtt.event.PublishDroppedEvent;
 import com.hivemq.mqtt.message.ProtocolVersion;
 import com.hivemq.mqtt.message.dropping.MessageDroppedService;
 import com.hivemq.mqtt.message.puback.PUBACK;
 import com.hivemq.mqtt.message.publish.PUBLISH;
 import com.hivemq.util.ChannelAttributes;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPromise;
 import io.netty.channel.embedded.EmbeddedChannel;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.exporter.ZipExporter;
@@ -41,6 +46,7 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import util.CollectUserEventsHandler;
 import util.TestConfigurationBootstrap;
 import util.TestMessageUtil;
 
@@ -48,6 +54,7 @@ import java.io.File;
 import java.net.URL;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
@@ -88,7 +95,11 @@ public class PublishOutboundInterceptorHandlerTest {
         channel.attr(ChannelAttributes.CLIENT_ID).set("test_client");
         configurationService = new TestConfigurationBootstrap().getFullConfigurationService();
 
-        handler = new PublishOutboundInterceptorHandler(asyncer, configurationService, pluginTaskExecutorService, hiveMQExtensions, messageDroppedService);
+        handler = new PublishOutboundInterceptorHandler(asyncer,
+                configurationService,
+                pluginTaskExecutorService,
+                hiveMQExtensions,
+                messageDroppedService);
         channel.pipeline().addLast(handler);
     }
 
@@ -126,6 +137,44 @@ public class PublishOutboundInterceptorHandlerTest {
         assertNotNull(publish);
     }
 
+    @Test(timeout = 5_000)
+    public void test_extension_prevented() throws Exception {
+        final PublishOutboundInterceptor interceptor = getIsolatedInterceptor();
+        when(clientContext.getPublishOutboundInterceptors()).thenReturn(ImmutableList.of(interceptor));
+
+        final CollectUserEventsHandler<PublishDroppedEvent> events =
+                new CollectUserEventsHandler<>(PublishDroppedEvent.class);
+        channel.pipeline().addLast(events);
+
+        final PublishOutboundOutputImpl publishOutboundOutput = new PublishOutboundOutputImpl(null, null);
+        publishOutboundOutput.preventPublishDelivery();
+
+        final ChannelHandlerContext ctx = channel.pipeline().context(PublishOutboundInterceptorHandler.class);
+
+        final PUBLISH publish = TestMessageUtil.createMqtt5Publish();
+
+        final ChannelPromise promise = channel.newPromise();
+        final PublishOutboundInterceptorHandler.PublishOutboundInterceptorContext context =
+                new PublishOutboundInterceptorHandler.PublishOutboundInterceptorContext("client",
+                        1,
+                        ctx,
+                        promise,
+                        publish,
+                        null,
+                        new ExtensionParameterHolder<>(publishOutboundOutput),
+                        mock(MessageDroppedService.class));
+
+        context.run();
+
+
+        PublishDroppedEvent publishDroppedEvent = events.pollEvent();
+        while (publishDroppedEvent == null) {
+            Thread.sleep(1);
+            publishDroppedEvent = events.pollEvent();
+        }
+        assertNotNull(publishDroppedEvent);
+        assertTrue(promise.isSuccess());
+    }
 
     private PublishOutboundInterceptor getIsolatedInterceptor() throws Exception {
 
@@ -137,9 +186,11 @@ public class PublishOutboundInterceptorHandlerTest {
         javaArchive.as(ZipExporter.class).exportTo(jarFile, true);
 
         //This classloader contains the classes from the jar file
-        final IsolatedPluginClassloader cl = new IsolatedPluginClassloader(new URL[]{jarFile.toURI().toURL()}, this.getClass().getClassLoader());
+        final IsolatedPluginClassloader cl =
+                new IsolatedPluginClassloader(new URL[]{jarFile.toURI().toURL()}, this.getClass().getClassLoader());
 
-        final Class<?> classOne = cl.loadClass("com.hivemq.extensions.handler.PublishOutboundInterceptorHandlerTest$TestInterceptor");
+        final Class<?> classOne =
+                cl.loadClass("com.hivemq.extensions.handler.PublishOutboundInterceptorHandlerTest$TestInterceptor");
 
         return (PublishOutboundInterceptor) classOne.newInstance();
     }
