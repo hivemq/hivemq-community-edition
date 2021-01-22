0d148f174340dd4b776e9e2fbfa2f6b63efd2b2c
DC2-DanielKrueger <daniel.krueger@dc-square.de>
2021-01-22 12:31:36 +0100

    fixed renaming

--- src/main/java/com/hivemq/bootstrap/netty/initializer/AbstractChannelInitializer.java
+++ src/main/java/com/hivemq/bootstrap/netty/initializer/AbstractChannelInitializer.java
@@ -78,8 +78,8 @@ public abstract class AbstractChannelInitializer extends ChannelInitializer<Chan
         // before connack outbound interceptor as it initializes the client context after the connack
         ch.pipeline().addLast(PLUGIN_INITIALIZER_HANDLER, channelDependencies.getPluginInitializerHandler());
 
-        ch.pipeline().addLast(CHANNEL_OUTBOUND_INTERCEPTOR_MULTIPLEXER, channelDependencies.getOutboundInterceptorHandler());
-        ch.pipeline().addLast(CHANNEL_INBOUND_INTERCEPTOR_MULTIPLEXER, channelDependencies.getInboundInterceptorHandler());
+        ch.pipeline().addLast(OUTBOUND_INTERCEPTOR, channelDependencies.getOutboundInterceptorHandler());
+        ch.pipeline().addLast(INBOUND_INTERCEPTOR, channelDependencies.getInboundInterceptorHandler());
 
         // after connect inbound interceptor as it intercepts the connect
         ch.pipeline().addLast(CLIENT_LIFECYCLE_EVENT_HANDLER, channelDependencies.getClientLifecycleEventHandler());
