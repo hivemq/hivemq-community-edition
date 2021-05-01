4ab6145e28c93cd06d1e8aa06860328e1fed03db
Silvio Giebl <silvio.giebl@hivemq.com>
2021-05-01 20:23:28 +0200

    build > adapted README.adoc, CONTRIBUTING.adoc and docker/build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -12,7 +12,7 @@ fi
 cd "$(dirname $0)/../"
 HIVEMQ_VERSION=$(./gradlew properties | grep ^version: | sed -e "s/version: //")
 echo "Building Docker image for HiveMQ ${HIVEMQ_VERSION}"
-./gradlew packaging
+./gradlew
 cd docker
 cp ../build/zip/hivemq-ce-${HIVEMQ_VERSION}.zip .
 docker build --build-arg HIVEMQ_VERSION=${HIVEMQ_VERSION} -f Dockerfile -t ${IMAGE} .
