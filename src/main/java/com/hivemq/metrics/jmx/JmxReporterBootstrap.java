f047cb579a351f8150834eb47ee10ecbbd37642f
A-Imal <Abdullah.Imal@dc-square.de>
2021-06-06 23:16:23 +0200

    Define explicit object name creation for JMX metric reporter of Dropwizard.

--- src/main/java/com/hivemq/metrics/jmx/JmxReporterBootstrap.java
+++ src/main/java/com/hivemq/metrics/jmx/JmxReporterBootstrap.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.hivemq.metrics.jmx;
 
 import com.codahale.metrics.MetricRegistry;
@@ -25,6 +26,8 @@ import org.slf4j.LoggerFactory;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
 
 /**
  * @author Lukas Brandl
@@ -42,7 +45,6 @@ public class JmxReporterBootstrap {
     @Inject
     public JmxReporterBootstrap(final MetricRegistry metricRegistry) {
         this.metricRegistry = metricRegistry;
-
     }
 
     @PostConstruct
@@ -50,7 +52,32 @@ public class JmxReporterBootstrap {
         if (!InternalConfigurations.JMX_REPORTER_ENABLED.get()) {
             return;
         }
-        jmxReporter = JmxReporter.forRegistry(metricRegistry).build();
+        /*
+         * The default object name factory in Dropwizard Metrics for JMX reporter was updated and changes the format.
+         * We need to support the old format as our customers are using them for their integrations.
+         * <p>
+         * - Old name format, that we support:  "metrics:name=kafka-extension.total.success.count"
+         * - New name format:                   "metrics:name=kafka-extension.total.success.count,type=counters"
+         * <p>
+         * The behavior was changed in this commit: https://github.com/dropwizard/metrics/pull/1310/files
+         * The code below was copied also from this commit.
+         */
+        jmxReporter = JmxReporter.forRegistry(metricRegistry).createsObjectNamesWith((type, domain, name) -> {
+            try {
+                ObjectName objectName = new ObjectName(domain, "name", name);
+                if (objectName.isPattern()) {
+                    objectName = new ObjectName(domain, "name", ObjectName.quote(name));
+                }
+                return objectName;
+            } catch (final MalformedObjectNameException e) {
+                try {
+                    return new ObjectName(domain, "name", ObjectName.quote(name));
+                } catch (final MalformedObjectNameException e1) {
+                    log.warn("Unable to register {} {}", type, name, e1);
+                    throw new RuntimeException(e1);
+                }
+            }
+        }).build();
         jmxReporter.start();
         log.debug("Started JMX Metrics Reporting.");
     }
