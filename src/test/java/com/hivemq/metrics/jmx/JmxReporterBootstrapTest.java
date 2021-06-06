77bcbf71c2a8b50531033fefca8d179d581853de
A-Imal <Abdullah.Imal@dc-square.de>
2021-06-06 23:16:23 +0200

    Define explicit object name creation for JMX metric reporter of Dropwizard.

--- src/test/java/com/hivemq/metrics/jmx/JmxReporterBootstrapTest.java
+++ src/test/java/com/hivemq/metrics/jmx/JmxReporterBootstrapTest.java
@@ -13,11 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.hivemq.metrics.jmx;
 
 import com.codahale.metrics.MetricRegistry;
 import org.junit.Test;
 
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
@@ -30,5 +37,63 @@ public class JmxReporterBootstrapTest {
         final JmxReporterBootstrap jmxReporterBootstrap = new JmxReporterBootstrap(new MetricRegistry());
         jmxReporterBootstrap.postConstruct();
         assertNotNull(jmxReporterBootstrap.jmxReporter);
+    }
+
+    @Test
+    public void objectNameWhenCounterMetricRequestedThenHasExpectedFormat() throws Exception {
+        final MetricRegistry metricRegistry = new MetricRegistry();
+        final JmxReporterBootstrap jmxReporterBootstrap = new JmxReporterBootstrap(metricRegistry);
+        jmxReporterBootstrap.postConstruct();
+
+        metricRegistry.counter("my-counter").inc();
+
+        final MBeanServer platformMBeanServer = ManagementFactory.getPlatformMBeanServer();
+        final ObjectName metricNameObject = new ObjectName("metrics:name=my-counter");
+        final Object attribute = platformMBeanServer.getAttribute(metricNameObject, "Count");
+        assertNotNull(attribute);
+
+        final double metricValue = Double.parseDouble(attribute.toString());
+        assertEquals(1, metricValue, 0);
+
+        jmxReporterBootstrap.stop();
+    }
+
+    @Test
+    public void objectNameWhenGaugeMetricRequestedThenHasExpectedFormat() throws Exception {
+        final MetricRegistry metricRegistry = new MetricRegistry();
+        final JmxReporterBootstrap jmxReporterBootstrap = new JmxReporterBootstrap(metricRegistry);
+        jmxReporterBootstrap.postConstruct();
+
+        final AtomicInteger integer = new AtomicInteger(0);
+        metricRegistry.gauge("my-gauge", () -> () -> integer.incrementAndGet());
+
+        final MBeanServer platformMBeanServer = ManagementFactory.getPlatformMBeanServer();
+        final ObjectName metricNameObject = new ObjectName("metrics:name=my-gauge");
+        final Object attribute = platformMBeanServer.getAttribute(metricNameObject, "Value");
+        assertNotNull(attribute);
+
+        final double metricValue = Double.parseDouble(attribute.toString());
+        assertEquals(integer.get(), metricValue, 0);
+
+        jmxReporterBootstrap.stop();
+    }
+
+    @Test
+    public void objectNameWhenHistogramMetricRequestedThenHasExpectedFormat() throws Exception {
+        final MetricRegistry metricRegistry = new MetricRegistry();
+        final JmxReporterBootstrap jmxReporterBootstrap = new JmxReporterBootstrap(metricRegistry);
+        jmxReporterBootstrap.postConstruct();
+
+        metricRegistry.histogram("my-histogram").update(1);
+
+        final MBeanServer platformMBeanServer = ManagementFactory.getPlatformMBeanServer();
+        final ObjectName metricNameObject = new ObjectName("metrics:name=my-histogram");
+        final Object attribute = platformMBeanServer.getAttribute(metricNameObject, "Mean");
+        assertNotNull(attribute);
+
+        final double metricValue = Double.parseDouble(attribute.toString());
+        assertEquals(1, metricValue, 0);
+
+        jmxReporterBootstrap.stop();
     }
 }
