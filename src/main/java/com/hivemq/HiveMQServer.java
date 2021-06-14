802e4ff1f36a7656d6cabdaf024cc7760cebe8a8
A-Imal <Abdullah.Imal@dc-square.de>
2021-06-14 17:51:50 +0200

    Reworked the HiveMQ start that the same code can be used also for EmbeddedHiveMQ.

--- src/main/java/com/hivemq/HiveMQServer.java
+++ src/main/java/com/hivemq/HiveMQServer.java
@@ -16,41 +16,36 @@
 package com.hivemq;
 
 import com.codahale.metrics.MetricRegistry;
-import com.google.common.util.concurrent.ListenableFuture;
 import com.google.inject.Injector;
-import com.hivemq.bootstrap.*;
+import com.hivemq.bootstrap.HiveMQExceptionHandlerBootstrap;
+import com.hivemq.bootstrap.LoggingBootstrap;
 import com.hivemq.bootstrap.ioc.GuiceBootstrap;
 import com.hivemq.common.shutdown.ShutdownHooks;
 import com.hivemq.configuration.ConfigurationBootstrap;
 import com.hivemq.configuration.HivemqId;
+import com.hivemq.configuration.info.SystemInformation;
 import com.hivemq.configuration.info.SystemInformationImpl;
 import com.hivemq.configuration.service.FullConfigurationService;
 import com.hivemq.configuration.service.InternalConfigurations;
 import com.hivemq.embedded.EmbeddedExtension;
+import com.hivemq.exceptions.StartAbortedException;
+import com.hivemq.exceptions.UnrecoverableException;
 import com.hivemq.extension.sdk.api.annotations.NotNull;
 import com.hivemq.extension.sdk.api.annotations.Nullable;
-import com.hivemq.extension.sdk.api.services.admin.AdminService;
-import com.hivemq.extensions.ExtensionBootstrap;
-import com.hivemq.extensions.services.admin.AdminServiceImpl;
 import com.hivemq.lifecycle.LifecycleModule;
 import com.hivemq.metrics.MetricRegistryLogger;
 import com.hivemq.migration.MigrationUnit;
 import com.hivemq.migration.Migrations;
 import com.hivemq.migration.meta.PersistenceType;
 import com.hivemq.persistence.PersistenceStartup;
-import com.hivemq.persistence.payload.PublishPayloadPersistence;
 import com.hivemq.statistics.UsageStatistics;
-import com.hivemq.util.Checkpoints;
 import com.hivemq.util.TemporaryFileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.inject.Inject;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 import static com.hivemq.configuration.service.PersistenceConfigurationService.PersistenceMode;
@@ -63,56 +58,52 @@ public class HiveMQServer {
 
     private static final Logger log = LoggerFactory.getLogger(HiveMQServer.class);
 
-    private final @NotNull HiveMQNettyBootstrap nettyBootstrap;
-    private final @NotNull PublishPayloadPersistence payloadPersistence;
-    private final @NotNull ExtensionBootstrap extensionBootstrap;
-    private final @NotNull AdminService adminService;
+    private final @NotNull HivemqId hivemqId;
+    private final @NotNull LifecycleModule lifecycleModule;
+    private final @NotNull SystemInformation systemInformation;
+    private final @NotNull MetricRegistry metricRegistry;
+    private final boolean migrate;
 
-    @Inject
-    HiveMQServer(
-            final @NotNull HiveMQNettyBootstrap nettyBootstrap,
-            final @NotNull PublishPayloadPersistence payloadPersistence,
-            final @NotNull ExtensionBootstrap extensionBootstrap,
-            final @NotNull AdminService adminService) {
+    private @Nullable Injector injector;
+    private @Nullable FullConfigurationService configService;
 
-        this.nettyBootstrap = nettyBootstrap;
-        this.payloadPersistence = payloadPersistence;
-        this.extensionBootstrap = extensionBootstrap;
-        this.adminService = adminService;
+    public HiveMQServer() {
+        this(
+                new SystemInformationImpl(true),
+                new MetricRegistry(),
+                null,
+                true
+        );
     }
 
-    public void start(final @Nullable EmbeddedExtension embeddedExtension) throws Exception {
-
-        payloadPersistence.init();
-
-        final CompletableFuture<Void> extensionStartFuture = extensionBootstrap.startExtensionSystem(embeddedExtension);
-        extensionStartFuture.get();
-
-        final ListenableFuture<List<ListenerStartupInformation>> startFuture = nettyBootstrap.bootstrapServer();
-
-        final List<ListenerStartupInformation> startupInformation = startFuture.get();
-        Checkpoints.checkpoint("listener-started");
-
-        new StartupListenerVerifier(startupInformation).verifyAndPrint();
-
-        ((AdminServiceImpl) adminService).hivemqStarted();
+    public HiveMQServer(
+            final @NotNull SystemInformation systemInformation,
+            final @Nullable MetricRegistry metricRegistry,
+            final @Nullable FullConfigurationService configService,
+            final boolean migrate) {
+        hivemqId = new HivemqId();
+        lifecycleModule = new LifecycleModule();
+        this.systemInformation = systemInformation;
+        this.metricRegistry = metricRegistry;
+        this.configService = configService;
+        this.migrate = migrate;
     }
 
-    public static void main(final @NotNull String[] args) throws Exception {
+    public void bootstrap() throws Exception {
+        // Already bootstrapped.
+        if (injector != null) {
+            return;
+        }
 
-        final long startTime = System.nanoTime();
-
-        final MetricRegistry metricRegistry = new MetricRegistry();
         metricRegistry.addListener(new MetricRegistryLogger());
 
-        final SystemInformationImpl systemInformation;
         LoggingBootstrap.prepareLogging();
 
         log.info("Starting HiveMQ Community Edition Server");
 
         log.trace("Initializing HiveMQ home directory");
         //Create SystemInformation this early because logging depends on it
-        systemInformation = new SystemInformationImpl(true);
+        systemInformation.init();
 
         log.trace("Initializing Logging");
         LoggingBootstrap.initLogging(systemInformation.getConfigFolder());
@@ -121,10 +112,11 @@ public class HiveMQServer {
         HiveMQExceptionHandlerBootstrap.addUnrecoverableExceptionHandler();
 
         log.trace("Initializing configuration");
-        final FullConfigurationService configService = ConfigurationBootstrap.bootstrapConfig(systemInformation);
+        if (configService == null) {
+            configService = ConfigurationBootstrap.bootstrapConfig(systemInformation);
+        }
 
-        final HivemqId hiveMQId = new HivemqId();
-        log.info("This HiveMQ ID is {}", hiveMQId.get());
+        log.info("This HiveMQ ID is {}", hivemqId.get());
 
         //ungraceful shutdown does not delete tmp folders, so we clean them up on broker start
         log.trace("Cleaning up temporary folders");
@@ -135,23 +127,21 @@ public class HiveMQServer {
         final Map<MigrationUnit, PersistenceType> migrations = Migrations.checkForTypeMigration(systemInformation);
         final Set<MigrationUnit> valueMigrations = Migrations.checkForValueMigration(systemInformation);
 
-        final LifecycleModule lifecycleModule = new LifecycleModule();
-
         log.trace("Initializing persistences");
         final Injector persistenceInjector =
-                GuiceBootstrap.persistenceInjector(systemInformation, metricRegistry, hiveMQId, configService,
+                GuiceBootstrap.persistenceInjector(systemInformation, metricRegistry, hivemqId, configService,
                         lifecycleModule);
         //blocks until all persistences started
         persistenceInjector.getInstance(PersistenceStartup.class).finish();
-        final ShutdownHooks shutdownHooks = persistenceInjector.getInstance(ShutdownHooks.class);
 
-        if (shutdownHooks.isShuttingDown()) {
-            return;
+        if (persistenceInjector.getInstance(ShutdownHooks.class).isShuttingDown()) {
+            throw new StartAbortedException();
         }
-        if (configService.persistenceConfigurationService().getMode() != PersistenceMode.IN_MEMORY) {
+
+        if (migrate && configService.persistenceConfigurationService().getMode() != PersistenceMode.IN_MEMORY) {
 
             if (migrations.size() + valueMigrations.size() > 0) {
-                if(migrations.size() > 0) {
+                if (migrations.size() > 0) {
                     log.info("Persistence types has been changed, migrating persistent data.");
                 } else {
                     log.info("Persistence values has been changed, migrating persistent data.");
@@ -166,22 +156,30 @@ public class HiveMQServer {
             }
 
             Migrations.afterMigration(systemInformation);
+        }
 
+        if (configService.persistenceConfigurationService().getMode().equals(PersistenceMode.FILE)) {
+            log.info("Starting with file persistence mode.");
         } else {
-            log.info("Starting with in memory persistences");
+            log.info("Starting with in-memory persistence mode.");
         }
 
         log.trace("Initializing Guice");
-        final Injector injector = GuiceBootstrap.bootstrapInjector(systemInformation, metricRegistry, hiveMQId,
+        injector = GuiceBootstrap.bootstrapInjector(systemInformation, metricRegistry, hivemqId,
                 configService, persistenceInjector, lifecycleModule);
+    }
+
+    public void start(final @Nullable EmbeddedExtension embeddedExtension) throws Exception {
         if (injector == null) {
-            return;
+            throw new UnrecoverableException(true);
         }
 
+        final ShutdownHooks shutdownHooks = injector.getInstance(ShutdownHooks.class);
         if (shutdownHooks.isShuttingDown()) {
-            return;
+            throw new StartAbortedException();
         }
-        final HiveMQServer instance = injector.getInstance(HiveMQServer.class);
+
+        final HiveMQInstance instance = injector.getInstance(HiveMQInstance.class);
 
         if (InternalConfigurations.GC_AFTER_STARTUP) {
             log.trace("Starting initial garbage collection after startup");
@@ -192,25 +190,50 @@ public class HiveMQServer {
         }
 
         if (shutdownHooks.isShuttingDown()) {
-            return;
+            throw new StartAbortedException();
         }
 
         /* It's important that we are modifying the log levels after Guice is initialized,
         otherwise this somehow interferes with Singleton creation */
         LoggingBootstrap.addLoglevelModifiers();
-        instance.start(null);
+        instance.start(embeddedExtension);
+    }
 
-        if (shutdownHooks.isShuttingDown()) {
+    public void afterStart() {
+        if (injector == null) {
             return;
         }
-
-        log.info("Started HiveMQ in {}ms", TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startTime));
-
+        final ShutdownHooks shutdownHooks = injector.getInstance(ShutdownHooks.class);
         if (shutdownHooks.isShuttingDown()) {
-            return;
+            throw new StartAbortedException();
         }
 
         final UsageStatistics usageStatistics = injector.getInstance(UsageStatistics.class);
         usageStatistics.start();
+    }
+
+    public void startFully() throws Exception {
+
+        final long startTime = System.nanoTime();
+
+        bootstrap();
+        start(null);
+
+        log.info("Started HiveMQ in {}ms", TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startTime));
+
+        afterStart();
+    }
+
+    public static void main(final String @NotNull [] args) throws Exception {
+        final HiveMQServer server = new HiveMQServer();
+        try {
+            server.startFully();
+        } catch (final StartAbortedException ignored) {
+            // This exception is ignored as we throw it ourselves when HiveMQ shutdown is initiated during the startup.
+        }
+    }
+
+    public @Nullable Injector getInjector() {
+        return injector;
     }
 }
