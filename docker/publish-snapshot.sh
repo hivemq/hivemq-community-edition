42342f669098e29b8f61f8aab46cc1b93fb2ed03
yweber <yannick.weber@hivemq.com>
2021-07-07 08:22:52 +0200

    improved docker image build scripts

--- docker/publish-snapshot.sh
+++ docker/publish-snapshot.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-IMAGE=${TARGET_IMAGE:-hivemq/hivemq-ce:snapshot}
+IMAGE="hivemq/hivemq-ce:snapshot"
 
 cd "$(dirname $0)/../"
 HIVEMQ_VERSION=$(./gradlew properties | grep ^version: | sed -e "s/version: //")
@@ -12,11 +12,6 @@ cd docker
 cp ../build/zip/hivemq-ce-${HIVEMQ_VERSION}.zip .
 docker build --build-arg HIVEMQ_VERSION=${HIVEMQ_VERSION} -f Dockerfile -t ${IMAGE} .
 rm -f hivemq-ce-${HIVEMQ_VERSION}.zip
-
-echo "Tagging image as snapshot"
-TAGGED_IMAGE="${IMAGE//:*}:snapshot"
-docker tag ${IMAGE} "${TAGGED_IMAGE}"
-
 
 if [[ ${PUSH_IMAGE} == true ]]; then
     echo "Pushing image"
