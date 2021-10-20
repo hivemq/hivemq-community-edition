9d565070c2fb68f13d927094de4543e193823eea
A-Imal <Abdullah.Imal@hivemq.com>
2021-10-20 11:08:44 +0200

    Use MqttServerDisconnector to disconnect client in NoConnectIdleHandler.

--- src/main/java/com/hivemq/mqtt/handler/connect/NoConnectIdleHandler.java
+++ src/main/java/com/hivemq/mqtt/handler/connect/NoConnectIdleHandler.java
@@ -16,13 +16,9 @@
 package com.hivemq.mqtt.handler.connect;
 
 import com.google.inject.Inject;
-import com.hivemq.bootstrap.ClientConnection;
-import com.hivemq.bootstrap.ClientState;
 import com.hivemq.extension.sdk.api.annotations.NotNull;
-import com.hivemq.logging.EventLog;
+import com.hivemq.mqtt.handler.disconnect.MqttServerDisconnector;
 import com.hivemq.mqtt.message.connect.CONNECT;
-import com.hivemq.util.ChannelAttributes;
-import com.hivemq.util.ChannelUtils;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
@@ -45,11 +41,12 @@ import static com.hivemq.bootstrap.netty.ChannelHandlerNames.NEW_CONNECTION_IDLE
 public class NoConnectIdleHandler extends ChannelInboundHandlerAdapter {
 
     private static final Logger log = LoggerFactory.getLogger(NoConnectIdleHandler.class);
-    private final @NotNull EventLog eventLog;
+
+    private final @NotNull MqttServerDisconnector mqttServerDisconnector;
 
     @Inject
-    public NoConnectIdleHandler(final @NotNull EventLog eventLog) {
-        this.eventLog = eventLog;
+    public NoConnectIdleHandler(final @NotNull MqttServerDisconnector mqttServerDisconnector) {
+        this.mqttServerDisconnector = mqttServerDisconnector;
     }
 
     @Override
@@ -58,9 +55,9 @@ public class NoConnectIdleHandler extends ChannelInboundHandlerAdapter {
             try {
                 ctx.pipeline().remove(NEW_CONNECTION_IDLE_HANDLER);
                 ctx.pipeline().remove(this);
-            } catch (final NoSuchElementException ex) {
+            } catch (final NoSuchElementException ignored) {
                 //no problem, because if these handlers are not in the pipeline anyway, we still get the expected result here
-                log.trace("Not able to remove no connect idle handler");
+                log.trace("Not able to remove no connect idle handler.");
             }
         }
         ctx.fireChannelRead(msg);
@@ -70,17 +67,9 @@ public class NoConnectIdleHandler extends ChannelInboundHandlerAdapter {
     public void userEventTriggered(final @NotNull ChannelHandlerContext ctx, final @NotNull Object evt) {
 
         if (evt instanceof IdleStateEvent && ((IdleStateEvent) evt).state() == IdleState.READER_IDLE) {
-            final ClientConnection clientConnection = ctx.channel().attr(ChannelAttributes.CLIENT_CONNECTION).get();
-            clientConnection.proposeClientState(ClientState.DISCONNECTING);
-            if (log.isDebugEnabled()) {
-
-                log.debug("Client with IP {} disconnected. The client was idle for too long without sending a MQTT CONNECT packet",
-                        ChannelUtils.getChannelIP(ctx.channel()).or("UNKNOWN"));
-                eventLog.clientWasDisconnected(ctx.channel(), "No CONNECT sent in time");
-                clientConnection.proposeClientState(ClientState.DISCONNECTED_BY_SERVER);
-                ctx.close();
-                return;
-            }
+            mqttServerDisconnector.logAndClose(ctx.channel(),
+                    "Client with IP {} disconnected. The client was idle for too long without sending a MQTT CONNECT packet.",
+                    "No CONNECT sent in time");
         }
         ctx.fireUserEventTriggered(evt);
     }
