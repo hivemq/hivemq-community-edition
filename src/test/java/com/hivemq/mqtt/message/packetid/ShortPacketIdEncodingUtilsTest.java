cac46617c24a93fe61769a7c37f978a63f9e9a45
Henning Lohse <henning.lohse@hivemq.com>
2022-12-22 13:00:37 +0100

    Remove author from unit test

--- src/test/java/com/hivemq/mqtt/message/packetid/ShortPacketIdEncodingUtilsTest.java
+++ src/test/java/com/hivemq/mqtt/message/packetid/ShortPacketIdEncodingUtilsTest.java
@@ -5,9 +5,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
-/**
- * @author Henning Lohse
- */
 public class ShortPacketIdEncodingUtilsTest {
 
     @Test
