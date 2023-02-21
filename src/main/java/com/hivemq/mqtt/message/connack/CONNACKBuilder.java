ac8a7fdc27eeddcf138ec12e6efb5e28bf76c97d
A-Imal <abdullah.imal@hivemq.com>
2023-02-21 15:48:55 +0100

    Move the precondition check of CONNACK to the build method.

--- src/main/java/com/hivemq/mqtt/message/connack/CONNACKBuilder.java
+++ src/main/java/com/hivemq/mqtt/message/connack/CONNACKBuilder.java
@@ -1,13 +1,33 @@
+/*
+ * Copyright 2019-present HiveMQ GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hivemq.mqtt.message.connack;
 
+import com.hivemq.codec.encoder.mqtt5.UnsignedDataTypes;
 import com.hivemq.extension.sdk.api.annotations.NotNull;
 import com.hivemq.extension.sdk.api.annotations.Nullable;
 import com.hivemq.mqtt.message.QoS;
+import com.hivemq.mqtt.message.connect.Mqtt5CONNECT;
 import com.hivemq.mqtt.message.mqtt5.Mqtt5UserProperties;
 import com.hivemq.mqtt.message.reason.Mqtt5ConnAckReasonCode;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 import static com.hivemq.mqtt.message.connack.CONNACK.KEEP_ALIVE_NOT_SET;
 import static com.hivemq.mqtt.message.connack.CONNACK.SESSION_EXPIRY_NOT_SET;
 import static com.hivemq.mqtt.message.connack.Mqtt5CONNACK.*;
@@ -48,6 +68,45 @@ public class CONNACKBuilder {
 
     public @NotNull CONNACK build() {
 
+        checkArgument(receiveMaximum != 0, "Receive maximum must never be zero");
+
+        if (assignedClientIdentifier != null) {
+            checkArgument(UnsignedDataTypes.isUnsignedShort(assignedClientIdentifier.getBytes(StandardCharsets.UTF_8).length),
+                    "A client Id must never exceed 65.535 bytes");
+        }
+        if (authMethod != null) {
+            checkArgument(UnsignedDataTypes.isUnsignedShort(authMethod.getBytes(StandardCharsets.UTF_8).length),
+                    "An auth method must never exceed 65.535 bytes");
+        }
+        if (authData != null) {
+            checkNotNull(authMethod, "Auth method must be set if auth data is set");
+            checkArgument(UnsignedDataTypes.isUnsignedShort(authData.length),
+                    "An auth data must never exceed 65.535 bytes");
+        }
+        if (responseInformation != null) {
+            checkArgument(UnsignedDataTypes.isUnsignedShort(responseInformation.getBytes(StandardCharsets.UTF_8).length),
+                    "A response information must never exceed 65.535 bytes");
+        }
+        if (serverReference != null) {
+            checkArgument(UnsignedDataTypes.isUnsignedShort(serverReference.getBytes(StandardCharsets.UTF_8).length),
+                    "A server reference must never exceed 65.535 bytes");
+        }
+        if (sessionExpiryInterval != SESSION_EXPIRY_NOT_SET) {
+            checkArgument(UnsignedDataTypes.isUnsignedInt(sessionExpiryInterval),
+                    "A session expiry interval must never be larger than 4.294.967.296");
+        }
+
+        checkArgument(maximumPacketSize <= Mqtt5CONNECT.DEFAULT_MAXIMUM_PACKET_SIZE_NO_LIMIT,
+                "A maximum packet size must never be larger than 268.435.460");
+
+        checkArgument(UnsignedDataTypes.isUnsignedShort(topicAliasMaximum),
+                "A topic alias maximum must never be larger than 65.535");
+
+        if (serverKeepAlive != KEEP_ALIVE_NOT_SET) {
+            checkArgument(UnsignedDataTypes.isUnsignedShort(serverKeepAlive),
+                    "A server keep alive must never be larger than 65.535");
+        }
+
         if (mqtt3ConnAckReturnCode != null) {
             if (mqtt3ConnAckReturnCode != Mqtt3ConnAckReturnCode.ACCEPTED && sessionPresent) {
 
@@ -56,6 +115,8 @@ public class CONNACKBuilder {
             }
             mqtt5ConnAckReasonCode = Mqtt5ConnAckReasonCode.fromReturnCode(mqtt3ConnAckReturnCode);
         }
+
+        checkNotNull(mqtt5ConnAckReasonCode);
 
         return new CONNACK(mqtt5ConnAckReasonCode,
                 reasonString,
