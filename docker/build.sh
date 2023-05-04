81442e13ebdfc37d621403e5df9205099dc7fe1f
Simon Uhrmann <simon.uhrmann@th-deg.de>
2023-05-04 10:31:27 +0200

    Add buildx to build script & Github Actions of snapshot and publish - Github Actions: add steps qemu and buildx - build.sh: replace build cmd with `docker buildx ...` - build.sh: use arg `PUSH_IMAGE` as conditional argument in build command
    ❗️TODO:
    1) publish.yml commented out push to Maved for dev (untested!)
    2) changed Docker registry to mine (suhrmann/hivemq-ce)

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-IMAGE="hivemq/hivemq-ce:snapshot"
+IMAGE="suhrmann/hivemq-ce:snapshot"
 
 cd "$(dirname $0)/../"
 HIVEMQ_VERSION=$(./gradlew properties | grep ^version: | sed -e "s/version: //")
@@ -10,10 +10,17 @@ echo "Building Docker image for HiveMQ ${HIVEMQ_VERSION}"
 ./gradlew hivemqZip
 cd docker
 cp ../build/distributions/hivemq-ce-${HIVEMQ_VERSION}.zip .
-docker build --build-arg HIVEMQ_VERSION=${HIVEMQ_VERSION} -f Dockerfile -t ${IMAGE} .
-rm -f hivemq-ce-${HIVEMQ_VERSION}.zip
 
+PUSH=""
 if [[ ${PUSH_IMAGE} == true ]]; then
     echo "Pushing image"
-    docker push ${IMAGE}
+    PUSH="--push"
 fi
+
+PLATFORMS=linux/amd64,linux/arm64,linux/arm/v7  # check support with jre base image!
+docker buildx build ${PUSH} --platform ${PLATFORMS}  \
+    --build-arg HIVEMQ_VERSION=${HIVEMQ_VERSION}  \
+    -f Dockerfile  \
+    -t ${IMAGE} .
+
+rm -f hivemq-ce-${HIVEMQ_VERSION}.zip
--- docker/publish.sh
+++ docker/publish.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-IMAGE_PREFIX="hivemq/hivemq-ce"
+IMAGE_PREFIX="suhrmann/hivemq-ce"
 LATEST_IMAGE="${IMAGE_PREFIX}:latest"
 
 cd "$(dirname $0)/../"
@@ -11,16 +11,22 @@ echo "Building Docker image for HiveMQ ${HIVEMQ_VERSION}"
 ./gradlew hivemqZip
 cd docker
 cp ../build/distributions/hivemq-ce-${HIVEMQ_VERSION}.zip .
-docker build --build-arg HIVEMQ_VERSION=${HIVEMQ_VERSION} -f Dockerfile -t ${LATEST_IMAGE} .
-rm -f hivemq-ce-${HIVEMQ_VERSION}.zip
 
 echo "Tagging image as ${HIVEMQ_VERSION}"
 VERSIONED_IMAGE="${IMAGE_PREFIX}:${HIVEMQ_VERSION}"
-docker tag ${LATEST_IMAGE} "${VERSIONED_IMAGE}"
 
+PUSH=""
 if [[ ${PUSH_IMAGE} == true ]]; then
+    PUSH="--push"
     echo "Pushing image as ${VERSIONED_IMAGE}"
-    docker push ${VERSIONED_IMAGE}
     echo "Pushing image as ${LATEST_IMAGE}"
-    docker push ${LATEST_IMAGE}
 fi
+
+PLATFORMS=linux/amd64,linux/arm64,linux/arm/v7  # check support with jre base image!
+docker buildx build ${PUSH} --platform ${PLATFORMS}  \
+  --build-arg HIVEMQ_VERSION=${HIVEMQ_VERSION}  \
+  -f Dockerfile  \
+  -t ${LATEST_IMAGE}  \
+  -t ${VERSIONED_IMAGE} .
+
+rm -f hivemq-ce-${HIVEMQ_VERSION}.zip
