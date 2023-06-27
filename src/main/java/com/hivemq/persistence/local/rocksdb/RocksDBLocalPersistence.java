46ec75a446490cafde620511b9b525ac3898e8a5
A-Imal <abdullah.imal@hivemq.com>
2023-06-27 13:16:21 +0200

    RocksDBLocalPersistence > Reduce number of returns.

--- src/main/java/com/hivemq/persistence/local/rocksdb/RocksDBLocalPersistence.java
+++ src/main/java/com/hivemq/persistence/local/rocksdb/RocksDBLocalPersistence.java
@@ -188,21 +188,20 @@ public abstract class RocksDBLocalPersistence implements LocalPersistence, FileP
     }
 
     protected static long physicalMemory() {
-        final long heap = Runtime.getRuntime().maxMemory();
-        try {
-            final OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
-            if (!(operatingSystemMXBean instanceof com.sun.management.OperatingSystemMXBean)) {
-                return (long) (heap * 1.5);
-            }
-            final long physicalMemory =
-                    ((com.sun.management.OperatingSystemMXBean) operatingSystemMXBean).getTotalPhysicalMemorySize();
+        final OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
+        if (operatingSystemMXBean instanceof com.sun.management.OperatingSystemMXBean) {
+            final com.sun.management.OperatingSystemMXBean sunBeam =
+                    (com.sun.management.OperatingSystemMXBean) operatingSystemMXBean;
+
+            final long physicalMemory = sunBeam.getTotalPhysicalMemorySize();
             if (physicalMemory > 0) {
                 return physicalMemory;
             }
-            return (long) (heap * 1.5);
-        } catch (final Exception e) {
-            return (long) (heap * 1.5);
         }
+
+        final long heap = Runtime.getRuntime().maxMemory();
+        final double fallbackEstimation = 1.5;
+        return (long) (heap * fallbackEstimation);
     }
 
     /**
