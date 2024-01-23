565e517c3644f07d98be41821604f2da20faf5ab
Silvio Giebl <silvio.giebl@hivemq.com>
2024-01-23 21:43:09 +0100

    Sync docker-entrypoint.sh with enterprise

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,23 +3,21 @@
 set -eo pipefail
 
 # We set the bind address here to ensure HiveMQ uses the correct interface. Defaults to using the container hostname (which should be hardcoded in /etc/hosts)
-if [ -z "${HIVEMQ_BIND_ADDRESS}" ]; then
+if [[ -z "${HIVEMQ_BIND_ADDRESS}" ]]; then
     echo "Getting bind address from container hostname"
-    ADDR=$(getent hosts ${HOSTNAME} | grep -v 127.0.0.1 | awk '{ print $1 }' | head -n 1)
+    HIVEMQ_BIND_ADDRESS=$(getent hosts ${HOSTNAME} | grep -v 127.0.0.1 | awk '{ print $1 }' | head -n 1)
+    export HIVEMQ_BIND_ADDRESS
 else
     echo "HiveMQ bind address was overridden by environment variable (value: ${HIVEMQ_BIND_ADDRESS})"
-    ADDR=${HIVEMQ_BIND_ADDRESS}
 fi
 
 # Remove allow all extension if applicable
-
 if [[ "${HIVEMQ_ALLOW_ALL_CLIENTS}" != "true" ]]; then
     echo "Disabling allow all extension"
     rm -rf /opt/hivemq/extensions/hivemq-allow-all-extension &>/dev/null || true
 fi
 
-echo "set bind address from container hostname to ${ADDR}"
-export HIVEMQ_BIND_ADDRESS=${ADDR}
+echo "setting bind address to ${HIVEMQ_BIND_ADDRESS}"
 
 # Step down from root privilege, only when we're attempting to run HiveMQ though.
 if [[ "$1" = "/opt/hivemq/bin/run.sh" && "$(id -u)" = '0' && "${HIVEMQ_NO_ROOT_STEP_DOWN}" != "true" ]]; then
@@ -53,4 +51,4 @@ if [[ "$(id -u)" = "0" ]]; then
     chmod -R 700 /opt/hivemq/bin
 fi
 
-HIVEMQ_BIND_ADDRESS=${ADDR} ${exec_cmd} "$@"
+${exec_cmd} "$@"
