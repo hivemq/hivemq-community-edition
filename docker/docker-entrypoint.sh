baae657afd10e21fdc44778188a571647d69a08d
Silvio Giebl <silvio.giebl@hivemq.com>
2024-02-26 18:21:00 +0100

    Use gradle-oci - multi-arch: linux/amd64, linux/arm64/v8 - remove root step down, run directly as user 10000 - remove hivemq user and group, only use numeric uid 10000

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -19,23 +19,4 @@ fi
 
 echo "setting bind address to ${HIVEMQ_BIND_ADDRESS}"
 
-# Step down from root privilege, only when we're attempting to run HiveMQ though.
-if [[ "$1" = "/opt/hivemq/bin/run.sh" && "$(id -u)" = '0' && "${HIVEMQ_NO_ROOT_STEP_DOWN}" != "true" ]]; then
-    uid="hivemq"
-    gid="hivemq"
-    exec_cmd="exec chroot --skip-chdir --userspec=hivemq /"
-else
-    uid="$(id -u)"
-    gid="$(id -g)"
-    exec_cmd="exec"
-fi
-
-readonly uid
-readonly gid
-readonly exec_cmd
-
-if [[ "$(id -u)" = "0" ]]; then
-    find /opt \! -user "${uid}" -exec chown "${uid}" '{}' + || true
-fi
-
-${exec_cmd} "$@"
+exec "$@"
