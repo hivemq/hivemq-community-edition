513bb42539b5b669c06e6cdcf10de73d93e30125
Silvio Giebl <silvio.giebl@hivemq.com>
2024-04-26 23:17:14 +0200

    Cleanup single/double quotes in run scripts

--- src/distribution/bin/run.sh
+++ src/distribution/bin/run.sh
@@ -12,31 +12,31 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-echo -------------------------------------------------------------------------
-echo
+echo '-------------------------------------------------------------------------'
+echo ''
 echo '                  _    _  _              __  __   ____'
-echo '                 | |  | |(_)            |  \/  | / __ \ '
+echo '                 | |  | |(_)            |  \/  | / __ \'
 echo '                 | |__| | _ __   __ ___ | \  / || |  | |'
 echo '                 |  __  || |\ \ / // _ \| |\/| || |  | |'
 echo '                 | |  | || | \ V /|  __/| |  | || |__| |'
 echo '                 |_|  |_||_|  \_/  \___||_|  |_| \___\_\'
-echo
-echo "-------------------------------------------------------------------------"
-echo ""
-echo "  HiveMQ Start Script for Linux/Unix v1.14"
-echo ""
+echo ''
+echo '-------------------------------------------------------------------------'
+echo ''
+echo '  HiveMQ Start Script for Linux/Unix v1.14'
+echo ''
 
 echoerr() { printf "%s\n" "$*" >&2; }
 
 if ! hash java 2>/dev/null; then
-    echoerr "ERROR! You do not have the Java Runtime Environment installed, please install Java JRE from https://adoptium.net/?variant=openjdk11 and try again."
+    echoerr 'ERROR! You do not have the Java Runtime Environment installed, please install Java JRE from https://adoptium.net/?variant=openjdk11 and try again.'
     exit 1
 fi
 
 java_version=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | sed 's/\..*//')
 
 if [[ "$((java_version))" -lt 11 ]]; then
-    echoerr "HiveMQ requires at least Java version 11"
+    echoerr 'HiveMQ requires at least Java version 11'
     exit 1
 fi
 
@@ -49,13 +49,13 @@ JAVA_OPTS="$JAVA_OPTS --add-opens java.base/sun.nio.ch=ALL-UNNAMED"
 JAVA_OPTS="$JAVA_OPTS --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED"
 JAVA_OPTS="$JAVA_OPTS --add-exports java.base/jdk.internal.misc=ALL-UNNAMED"
 
-if [ -c "/dev/urandom" ]; then
+if [ -c '/dev/urandom' ]; then
     # Use /dev/urandom as standard source for secure randomness if it exists
     JAVA_OPTS="$JAVA_OPTS -Djava.security.egd=file:/dev/./urandom"
 fi
 
 # JMX Monitoring
-if [ "${HIVEMQ_JMX_ENABLED:-true}" == "true" ]; then
+if [ "${HIVEMQ_JMX_ENABLED:-true}" == 'true' ]; then
     JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=${HIVEMQ_JMX_PORT:-9010} -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
 fi
 
@@ -79,18 +79,18 @@ else
 fi
 
 if [ ! -d "$HIVEMQ_FOLDER" ]; then
-    echoerr "ERROR! HiveMQ Home Folder not found."
+    echoerr 'ERROR! HiveMQ Home Folder not found.'
     exit 1
 fi
 
 if [ ! -w "$HIVEMQ_FOLDER" ]; then
-    echoerr "ERROR! HiveMQ Home Folder Permissions not correct."
+    echoerr 'ERROR! HiveMQ Home Folder Permissions not correct.'
     exit 1
 fi
 
 JAR_PATH="$HIVEMQ_FOLDER/bin/hivemq.jar"
 if [ ! -f "$JAR_PATH" ]; then
-    echoerr "ERROR! HiveMQ JAR not found."
+    echoerr 'ERROR! HiveMQ JAR not found.'
     echoerr "$HIVEMQ_FOLDER"
     exit 1
 fi
@@ -100,15 +100,15 @@ JAVA_OPTS="$JAVA_OPTS -XX:+HeapDumpOnOutOfMemoryError"
 HEAPDUMP_PATH_OPT="-XX:HeapDumpPath=$HEAPDUMP_PATH/heap-dump.hprof"
 ERROR_FILE_PATH_OPT="-XX:ErrorFile=$HEAPDUMP_PATH/hs_err_pid%p.log"
 
-echo "-------------------------------------------------------------------------"
-echo ""
+echo '-------------------------------------------------------------------------'
+echo ''
 echo "  HIVEMQ_HOME: $HIVEMQ_FOLDER"
-echo ""
+echo ''
 echo "  JAVA_OPTS: $JAVA_OPTS"
-echo ""
+echo ''
 echo "  JAVA_VERSION: $java_version"
-echo ""
-echo "-------------------------------------------------------------------------"
-echo ""
+echo ''
+echo '-------------------------------------------------------------------------'
+echo ''
 
-exec "java" "${HOME_OPT}" "${HEAPDUMP_PATH_OPT}" "${ERROR_FILE_PATH_OPT}" ${JAVA_OPTS} -jar "${JAR_PATH}"
+exec java "${HOME_OPT}" "${HEAPDUMP_PATH_OPT}" "${ERROR_FILE_PATH_OPT}" ${JAVA_OPTS} -jar "${JAR_PATH}"
