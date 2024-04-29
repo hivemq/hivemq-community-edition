371e778a668c4bba40fe57b64a417792199ab3c0
Silvio Giebl <silvio.giebl@hivemq.com>
2024-04-29 13:15:19 +0200

    Improve batch run scripts

--- src/distribution/bin/diagnostics.bat
+++ src/distribution/bin/diagnostics.bat
@@ -12,121 +12,118 @@ rem  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 rem  See the License for the specific language governing permissions and
 rem  limitations under the License.
 
-  @echo off
-  echo -------------------------------------------------------------------------
-  echo.
-  echo                   _    _  _              __  __   ____
-  echo                  ^| ^|  ^| ^|(_)            ^|  \/  ^| / __ \
-  echo                  ^| ^|__^| ^| _ __   __ ___ ^| \  / ^|^| ^|  ^| ^|
-  echo                  ^|  __  ^|^| ^|\ \ / // _ \^| ^|\/^| ^|^| ^|  ^| ^|
-  echo                  ^| ^|  ^| ^|^| ^| \ V /^|  __/^| ^|  ^| ^|^| ^|__^| ^|
-  echo                  ^|_^|  ^|_^|^|_^|  \_/  \___^|^|_^|  ^|_^| \___\_\
-  echo.
-  echo -------------------------------------------------------------------------
-  echo.
-  echo   HiveMQ Start Script for Windows v1.6
-  echo.
-  echo                DIAGNOSTIC MODE
-  echo.
+@echo off
+setlocal
+echo -------------------------------------------------------------------------
+echo.
+echo                   _    _  _              __  __   ____
+echo                  ^| ^|  ^| ^|(_)            ^|  \/  ^| / __ \
+echo                  ^| ^|__^| ^| _ __   __ ___ ^| \  / ^|^| ^|  ^| ^|
+echo                  ^|  __  ^|^| ^|\ \ / // _ \^| ^|\/^| ^|^| ^|  ^| ^|
+echo                  ^| ^|  ^| ^|^| ^| \ V /^|  __/^| ^|  ^| ^|^| ^|__^| ^|
+echo                  ^|_^|  ^|_^|^|_^|  \_/  \___^|^|_^|  ^|_^| \___\_\
+echo.
+echo -------------------------------------------------------------------------
+echo.
+echo   HiveMQ Start Script for Windows v1.7
+echo.
+echo                DIAGNOSTIC MODE
+echo.
 
-  call :isAdmin
-  if %errorlevel% == 0 (
+call :isAdmin
+if %errorlevel% == 0 (
     echo.
     echo    Running with admin rights.
     echo.
-  ) else (
+) else (
     echo -------------------------------------------------------------------------
     echo.
     echo    WARNING
     echo.
     echo    HiveMQ needs more runtime privileges,
-    echo    please run again as admin. (right click, select 'Run as administrator')
+    echo    please run again as admin. (right click, select 'Run as administrator'^)
     echo.
     echo -------------------------------------------------------------------------
     echo.
-  )
+)
 
-  java -version >nul 2>&1
-  if errorlevel 1 goto NOJAVA
+java -version >nul 2>&1
+if errorlevel 1 (
+    echo ERROR! You do not have the Java Runtime Environment installed, please install Java JRE from https://adoptium.net/?variant=openjdk11 and try again.
+    goto EXIT
+)
 
-  for /f tokens^=2-2^ delims^=^" %%j in ('java -version 2^>^&1') do @set "java_version=%%j"
+for /f tokens^=2-2^ delims^=^" %%j in ('java -version 2^>^&1') do set "JAVA_VERSION=%%j"
 
-  for /F "tokens=1 delims=. " %%a in ("%java_version%") do (
-     set java_version_start=%%a
-  )
+for /f "tokens=1 delims=. " %%a in ("%JAVA_VERSION%") do set "JAVA_MAJOR_VERSION=%%a"
 
-  rem ########### VARIABLES
-  set "JAVA_OPTS=-Djava.net.preferIPv4Stack=true %JAVA_OPTS%"
+if %JAVA_MAJOR_VERSION% LSS 11 (
+    echo ERROR! HiveMQ requires at least Java version 11.
+    goto EXIT
+)
 
-  if %java_version_start% LEQ "10" (
-    echo ERROR! HiveMQ requires at least Java 11
-  )
+rem ########### VARIABLES
+set "JAVA_OPTS=%JAVA_OPTS% -Djava.net.preferIPv4Stack=true"
 
-  set "JAVA_OPTS=%JAVA_OPTS% --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.base/sun.security.provider=ALL-UNNAMED --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED --add-exports java.base/jdk.internal.misc=ALL-UNNAMED"
+set "JAVA_OPTS=%JAVA_OPTS% --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.base/sun.security.provider=ALL-UNNAMED --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED --add-exports java.base/jdk.internal.misc=ALL-UNNAMED"
 
-  rem JMX Monitoring
-  IF NOT DEFINED HIVEMQ_JMX_PORT SET "HIVEMQ_JMX_PORT=9010"
-  set "JAVA_OPTS=-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=%HIVEMQ_JMX_PORT% -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false %JAVA_OPTS%"
+rem JMX Monitoring
+if not defined HIVEMQ_JMX_ENABLED set "HIVEMQ_JMX_ENABLED=true"
+if not defined HIVEMQ_JMX_PORT set "HIVEMQ_JMX_PORT=9010"
+if "%HIVEMQ_JMX_ENABLED%" == "true" (
+    set "JAVA_OPTS=%JAVA_OPTS% -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=%HIVEMQ_JMX_PORT% -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+)
 
-  set "JAVA_OPTS=-DdiagnosticMode=true %JAVA_OPTS%"
+rem Disable Localization
+set "JAVA_OPTS=%JAVA_OPTS% -Duser.language=en -Duser.region=US"
+
+set "JAVA_OPTS=%JAVA_OPTS% -DdiagnosticMode=true"
+
+if not defined HIVEMQ_HOME (
+    rem %0 is the current script, ~ removes quotes, d includes the drive letter, p includes the path without the filename
+    call :RESOLVE "%~dp0\.." HIVEMQ_HOME
+)
+
+if not exist "%HIVEMQ_HOME%\" (
+    echo ERROR! HiveMQ home folder not found or readable.
+    goto EXIT
+)
+
+if not exist "%HIVEMQ_HOME%\bin\hivemq.jar" (
+    echo ERROR! HiveMQ JAR not found.
+    goto EXIT
+)
+
+if not defined HIVEMQ_HEAPDUMP_FOLDER set "HIVEMQ_HEAPDUMP_FOLDER=%HIVEMQ_HOME%"
+set "JAVA_OPTS=%JAVA_OPTS% -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath="%HIVEMQ_HEAPDUMP_FOLDER%\heap-dump.hprof""
+
+echo -------------------------------------------------------------------------
+echo.
+echo   HIVEMQ_HOME: %HIVEMQ_HOME%
+echo.
+echo   JAVA_OPTS: %JAVA_OPTS%
+echo.
+echo   JAVA_VERSION: %JAVA_VERSION%
+echo.
+echo -------------------------------------------------------------------------
+echo.
+
+set "JAVA_OPTS=-Dhivemq.home="%HIVEMQ_HOME%" %JAVA_OPTS%"
+
+java %JAVA_OPTS% -jar "%HIVEMQ_HOME%\bin\hivemq.jar"
+
+:EXIT
+pause
+goto :EOF
 
 
-  IF "%HIVEMQ_HOME%" == "" GOTO NOPATH
-  set HIVEMQ_FOLDER="%HIVEMQ_HOME%";
-  GOTO CHECKPATH
-
-:NOPATH
-  SET ROOT=%~dp0
-  rem Resolve is fetching the parent directory
-  CALL :RESOLVE "%ROOT%\.." PARENT_ROOT
-  set HIVEMQ_FOLDER="%PARENT_ROOT%"
-:CHECKPATH
-  set HIVEMQ_FOLDER=%HIVEMQ_FOLDER:;=%
-  rem Check if directory exists
-  FOR %%i IN (%HIVEMQ_FOLDER%) DO IF EXIST %%~si\NUL  GOTO CHECKFILE
-  echo ERROR! HiveMQ Home Folder not found or readable.
-  GOTO EXIT
-
-:CHECKFILE
-  rem Check if file exists
-  FOR %%i IN (%HIVEMQ_FOLDER%/bin/hivemq.jar) DO IF EXIST %%~si  GOTO START
-  echo ERROR! HiveMQ JAR not found.
-  GOTO EXIT
-
-:START
-      set "JAVA_OPTS=-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=%HIVEMQ_FOLDER%/heap-dump.hprof %JAVA_OPTS%"
-
-      echo -------------------------------------------------------------------------
-      echo.
-      echo   HIVEMQ_HOME: %HIVEMQ_FOLDER%
-      echo.
-      echo   JAVA_OPTS: %JAVA_OPTS%
-      echo.
-      echo   JAVA_VERSION: %java_version%
-      echo.
-      echo -------------------------------------------------------------------------
-      echo.
-
-  set "JAVA_OPTS=-Dhivemq.home=%HIVEMQ_FOLDER% %JAVA_OPTS%"
-
-  java %JAVA_OPTS% -jar %HIVEMQ_FOLDER%/bin/hivemq.jar
-  GOTO EXIT
-
-
+rem function that exits with 0 if running with admin rights
 :isAdmin
 fsutil dirty query %systemdrive% >nul
 exit /b
 
-:NOJAVA
-  echo You do not have the Java Runtime Environment installed, please install Java JRE from https://adoptium.net/?variant=openjdk11 and try again.
-
-:EXIT
-  pause
-
-:EXIT_WO_PAUSE
-
-GOTO :EOF
-
+rem function that resolves the path in arg 1 and stores it in the variable arg 2
 :RESOLVE
-SET %2=%~f1
-GOTO :EOF
+rem ~ removes quotes, f includes the fully qualified path
+set %2=%~f1
+goto :EOF
