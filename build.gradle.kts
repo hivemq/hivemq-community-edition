88913021086faf41484925ef5a37d95faebb232d
Silvio Giebl <silvio.giebl@hivemq.com>
2024-06-05 16:00:40 +0200

    Update eclipse-temurin digest

--- build.gradle.kts
+++ build.gradle.kts
@@ -229,7 +229,7 @@ oci {
         imageName = "hivemq/hivemq-ce"
         allPlatforms {
             parentImages {
-                add("library:eclipse-temurin:sha256!0f8bc645fb0c9ab40c913602c9f5f12c32d9ae6bef3e34fa0469c98e7341333c") // 21.0.3_9-jre-jammy
+                add("library:eclipse-temurin:sha256!78a82edcacc6cef9fd8c8a276fbd5e08f72fcdfbaf9d28df8b2d9207a7450cb6") // 21.0.3_9-jre-jammy
             }
             config {
                 user = "10000"
