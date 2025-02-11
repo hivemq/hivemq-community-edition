b1529d03bee03440356ea95302bb61c6ded722d9
Marcel Bartholet <18328939+Z0rdak@users.noreply.github.com>
2025-02-11 13:13:27 +0100

    add implementation for newly added interface methods

--- src/main/java/com/hivemq/configuration/service/impl/MqttConfigurationServiceImpl.java
+++ src/main/java/com/hivemq/configuration/service/impl/MqttConfigurationServiceImpl.java
@@ -33,6 +33,7 @@ import static com.hivemq.configuration.entity.mqtt.MqttConfigurationDefaults.KEE
 import static com.hivemq.configuration.entity.mqtt.MqttConfigurationDefaults.MAXIMUM_QOS_DEFAULT;
 import static com.hivemq.configuration.entity.mqtt.MqttConfigurationDefaults.MAX_EXPIRY_INTERVAL_DEFAULT;
 import static com.hivemq.configuration.entity.mqtt.MqttConfigurationDefaults.MAX_QUEUED_MESSAGES_DEFAULT;
+import static com.hivemq.configuration.entity.mqtt.MqttConfigurationDefaults.MQTT_ALLOW_DOLLAR_TOPICS_DEFAULT;
 import static com.hivemq.configuration.entity.mqtt.MqttConfigurationDefaults.QUEUED_MESSAGES_STRATEGY_DEFAULT;
 import static com.hivemq.configuration.entity.mqtt.MqttConfigurationDefaults.RETAINED_MESSAGES_ENABLED_DEFAULT;
 import static com.hivemq.configuration.entity.mqtt.MqttConfigurationDefaults.SERVER_RECEIVE_MAXIMUM_DEFAULT;
@@ -64,6 +65,8 @@ public class MqttConfigurationServiceImpl implements MqttConfigurationService {
 
     private final AtomicBoolean retainedMessagesEnabled = new AtomicBoolean(RETAINED_MESSAGES_ENABLED_DEFAULT);
 
+    private final AtomicBoolean mqttAllowDollarTopics = new AtomicBoolean(MQTT_ALLOW_DOLLAR_TOPICS_DEFAULT);
+    
     private final AtomicBoolean wildcardSubscriptionsEnabled =
             new AtomicBoolean(WILDCARD_SUBSCRIPTIONS_ENABLED_DEFAULT);
 
@@ -111,6 +114,9 @@ public class MqttConfigurationServiceImpl implements MqttConfigurationService {
     public boolean retainedMessagesEnabled() {
         return retainedMessagesEnabled.get();
     }
+
+    @Override
+    public boolean allowDollarTopicsEnabled() { return mqttAllowDollarTopics.get(); }
 
     @Override
     public boolean wildcardSubscriptionsEnabled() {
@@ -187,6 +193,12 @@ public class MqttConfigurationServiceImpl implements MqttConfigurationService {
     public void setRetainedMessagesEnabled(final boolean enabled) {
         log.debug("Setting retained messages enabled to {}", enabled);
         this.retainedMessagesEnabled.set(enabled);
+    }
+
+    @Override
+    public void setAllowDollarTopicsEnabled(final boolean enabled) {
+        log.debug("Setting allow dollar topics enabled to {}", enabled);
+        this.mqttAllowDollarTopics.set(enabled);
     }
 
     @Override
