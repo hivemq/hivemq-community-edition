9deb94680e4129f2a15eeb2aa5eb9c440e21f8bc
David Sondermann <david.sondermann@hivemq.com>
2026-02-13 10:38:27 +0100

    Remove HiveMQ extension SDK as an included build and update version to 4.48.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,1 @@
 rootProject.name = "hivemq-community-edition"
-
-if (file("../hivemq-extension-sdk").exists()) {
-    includeBuild("../hivemq-extension-sdk")
-} else {
-    logger.warn(
-        """
-        ######################################################################################################
-        You can not use the latest changes of or modify the hivemq-extension-sdk.
-        Please checkout the hivemq-extension-sdk repository next to the hivemq-community-edition repository.
-        Execute the following command from your project directory:
-        git clone https://github.com/hivemq/hivemq-extension-sdk.git ../hivemq-extension-sdk
-        You can also clone your fork:
-        git clone https://github.com/<replace-with-your-fork>/hivemq-extension-sdk.git ../hivemq-extension-sdk
-        ######################################################################################################
-        """.trimIndent()
-    )
-}
