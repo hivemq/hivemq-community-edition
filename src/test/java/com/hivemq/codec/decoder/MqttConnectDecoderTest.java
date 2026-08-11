1bda70bd764089105cf53b7c0d87197c22f40da7
Filipe Almeida <filipe.almeida@vocovo.com>
2026-08-11 14:50:43 +0100

    feat(PLAT-1336): support try_private flag in MQTT connect decoder (#1)
    * feat(PLAT-1336): support try_private flag in MQTT connect decoder
    
    * chore: add more relevant unit test
    
    * chore: run ./gradlew spotlessApply

--- src/test/java/com/hivemq/codec/decoder/MqttConnectDecoderTest.java
+++ src/test/java/com/hivemq/codec/decoder/MqttConnectDecoderTest.java
@@ -21,6 +21,7 @@ import com.hivemq.configuration.HivemqId;
 import com.hivemq.extension.sdk.api.annotations.NotNull;
 import com.hivemq.mqtt.handler.connack.MqttConnacker;
 import com.hivemq.mqtt.message.ProtocolVersion;
+import com.hivemq.mqtt.message.connect.CONNECT;
 import com.hivemq.mqtt.message.reason.Mqtt5ConnAckReasonCode;
 import com.hivemq.util.ClientIds;
 import io.netty.buffer.ByteBuf;
@@ -38,6 +39,7 @@ import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 public class MqttConnectDecoderTest {
 
@@ -141,4 +143,33 @@ public class MqttConnectDecoderTest {
                 eq(Mqtt5ConnAckReasonCode.UNSUPPORTED_PROTOCOL_VERSION),
                 anyString());
     }
+
+    @Test
+    public void decode_whenMosquittoTryPrivateProtocolVersion_thenSuccessfullyDecodedAsMqtt311() {
+        final ByteBuf buf = Unpooled.wrappedBuffer(new byte[]{0, 4, 'M', 'Q', 'T', 'T', (byte) 0x84, // Mosquitto try_private protocol level (0x84) (try_private = true)
+                2, // Clean session
+                0, 60, // Keep alive (60s)
+                0, 6, 'b', 'r', 'i', 'd', 'g', 'e' // Client ID
+        });
+
+        final CONNECT connect = decoder.decode(clientConnection, buf, FIXED_HEADER);
+        assertNotNull(connect);
+        assertSame(ProtocolVersion.MQTTv3_1_1, connect.getProtocolVersion());
+        verifyNoMoreInteractions(mqttConnacker);
+    }
+
+    @Test
+    public void decode_whenStandardProtocolVersion_thenSuccessfullyDecodedAsMqtt311() {
+        final ByteBuf buf = Unpooled.wrappedBuffer(new byte[]{0, 4, 'M', 'Q', 'T', 'T', (byte) 0x04, // Standard MQTT 3.1.1 protocol level (try_private = false)
+                2, // Clean session
+                0, 60, // Keep alive (60s)
+                0, 6, 'b', 'r', 'i', 'd', 'g', 'e' // Client ID
+        });
+
+        final CONNECT connect = decoder.decode(clientConnection, buf, FIXED_HEADER);
+        assertNotNull(connect);
+        assertSame(ProtocolVersion.MQTTv3_1_1, connect.getProtocolVersion());
+        verifyNoMoreInteractions(mqttConnacker);
+    }
+
 }
